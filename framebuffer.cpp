12c7394ac1aeed08bc295caf7fc7b1c45affa4fc
Dima Zavin <dima@android.com>
2009-08-07 15:58:07 -0700

    gralloc8k: make sure to set the physical address of the framebuffer
    Signed-off-by: Dima Zavin <dima@android.com>

--- framebuffer.cpp
+++ framebuffer.cpp
@@ -301,6 +301,7 @@ int mapFrameBufferLocked(struct private_module_t* module)
         return -errno;
     }
     module->framebuffer->base = intptr_t(vaddr);
+    module->framebuffer->phys = intptr_t(finfo.smem_start);
     memset(vaddr, 0, fbSize);
     return 0;
 }
