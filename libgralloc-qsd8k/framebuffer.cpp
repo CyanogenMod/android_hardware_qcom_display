6e83a3025103d43493457b4d240f11c377a1b7fb
Mathias Agopian <mathias@google.com>
2009-08-19 13:45:02 -0700

    reenabled partial updates in msm7k/qsd8k

--- libgralloc-qsd8k/framebuffer.cpp
+++ libgralloc-qsd8k/framebuffer.cpp
@@ -41,9 +41,6 @@
 
 /*****************************************************************************/
 
-// should be a build option
-#define SUPPORTS_UPDATE_ON_DEMAND   0
-
 // numbers of buffers for page flipping
 #define NUM_BUFFERS 2
 
@@ -377,13 +374,11 @@ int fb_device_open(hw_module_t const* module, const char* name,
             const_cast<int&>(dev->device.minSwapInterval) = 1;
             const_cast<int&>(dev->device.maxSwapInterval) = 1;
 
-#if SUPPORTS_UPDATE_ON_DEMAND
             if (m->finfo.reserved[0] == 0x5444 &&
                     m->finfo.reserved[1] == 0x5055) {
                 dev->device.setUpdateRect = fb_setUpdateRect;
                 LOGD("UPDATE_ON_DEMAND supported");
             }
-#endif
 
             *device = &dev->device.common;
         }
