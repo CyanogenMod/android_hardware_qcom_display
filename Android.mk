7bb2562ff29e940ada95e30c688c483d281a94a7
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2011-03-15 20:08:37 +0530

    Cleanup QCOM usage of LOCAL_MODULE_TAGS
    Starting from GB, modules which doesn't have LOCAL_MODULE_TAGS
    defined (default to user tag) or which has user tag and not part
    of GRANDFATHERED_USER_MODULES does throgh ERROR/WARNING
    message. To supress these WARNING messages, we need to add
    optional tag.
    
    Targets tested on:
    msm8660_surf
    
    Tests:
    Did a build from scratch
    
    Change-Id: I13484250258582dc7c05e1893712719660a8ff06

--- Android.mk
+++ Android.mk
@@ -30,6 +30,7 @@ LOCAL_SRC_FILES := 	\
 	pmemalloc.cpp
 	
 LOCAL_MODULE := gralloc.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS:= -DLOG_TAG=\"$(TARGET_BOARD_PLATFORM).gralloc\" -DHOST
 
 ifneq (, $(filter msm7625_ffa msm7625_surf msm7627_ffa msm7627_surf msm7627_7x_ffa msm7627_7x_surf, $(QCOM_TARGET_PRODUCT)))
