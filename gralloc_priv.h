b88364eaa326f59704402b021d55334aaf64367b
Arun Kumar K.R <akkr@codeaurora.org>
2011-04-06 18:30:28 -0700

    hardware/msm7k: Remove ioctl call from getOrientation
    - The ioctl call from getOrientation is removed.
    - In gralloc use getOrientation instead of storing the
      orientation.
    
    Change-Id: Idb08568ff5d46ce99e0a9a8c6f492b90bb666f0c

--- gralloc_priv.h
+++ gralloc_priv.h
@@ -228,7 +228,6 @@ struct private_module_t {
     bool exitHDMIUILoop;
     float actionsafeWidthRatio;
     float actionsafeHeightRatio;
-    int currentOrientation;
     bool hdmiStateChanged;
     pthread_mutex_t overlayLock;
     pthread_cond_t overlayPost;
