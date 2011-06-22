727d46f18bab7c6857d526dbe82a46c09b11831a
Saurabh Shah <saurshah@codeaurora.org>
2011-06-21 18:02:05 -0700

    Use reserved[4] field for storing fps
    Use reserved[4] field for storing fps, since reserved[3]
    is used by HDMI
    
    Change-Id: I4cc94eb0359b71bf15ae025e30fdad3fc96ae1fd

--- framebuffer.cpp
+++ framebuffer.cpp
@@ -851,8 +851,8 @@ int mapFrameBufferLocked(struct private_module_t* module)
 
     float xdpi = (info.xres * 25.4f) / info.width;
     float ydpi = (info.yres * 25.4f) / info.height;
-    //The reserved[3] field is used to store FPS by the driver.
-    float fps  = info.reserved[3];
+    //The reserved[4] field is used to store FPS by the driver.
+    float fps  = info.reserved[4];
 
     LOGI(   "using (fd=%d)\n"
             "id           = %s\n"
