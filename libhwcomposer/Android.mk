dd6a62c9dd0f2ef485f0c1bd2269c5ad5dcac1a8
Naseer Ahmed <naseer@quicinc.com>
2011-11-14 14:58:33 -0800

    hwc: Change Android.mk to point to new display gralloc
    Change-Id: Ie7c08fb55339c2c7b7bf2c28576291a5a1e1d809

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -5,14 +5,14 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils libEGL libhardware liboverlay
+LOCAL_SHARED_LIBRARIES := liblog libcutils libEGL libhardware libutils liboverlay
 
 LOCAL_SRC_FILES := 	\
 	hwcomposer.cpp
 	
 LOCAL_MODULE := hwcomposer.$(TARGET_BOARD_PLATFORM)
 LOCAL_CFLAGS:= -DLOG_TAG=\"$(TARGET_BOARD_PLATFORM).hwcomposer\"
-LOCAL_C_INCLUDES += hardware/msm7k/libgralloc-qsd8k
+LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
 LOCAL_C_INCLUDES += hardware/msm7k/liboverlay
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
