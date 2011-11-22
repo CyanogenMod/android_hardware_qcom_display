be1b5f3d66d063e7506ab32ad39556d3127031a5
Naomi Luis <nluis@codeaurora.org>
2011-11-21 21:01:30 -0800

    libhwcomposer: Use genlock to lock/unlock buffers for composition.
    For copybit composition, aquire a read lock on the buffer, before
    the blit operation and release the lock after the blit operation.
    
    For overlay, aquire a read lock on buffer N that is sent to the
    overlay. After the play call for buffer N returns, unlock the lock
    held by buffer (N-1).
    
    Change-Id: I3ff178d8ed8e3a289c199237dbec501afdb7e717

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -6,16 +6,20 @@ include $(CLEAR_VARS)
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libEGL libhardware libutils liboverlay
+LOCAL_SHARED_LIBRARIES += libgenlock
 
 LOCAL_SRC_FILES := 	\
 	hwcomposer.cpp
 	
 LOCAL_MODULE := hwcomposer.$(TARGET_BOARD_PLATFORM)
 LOCAL_CFLAGS:= -DLOG_TAG=\"$(TARGET_BOARD_PLATFORM).hwcomposer\"
+
 LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
 LOCAL_C_INCLUDES += hardware/qcom/display/liboverlay
 LOCAL_C_INCLUDES += hardware/qcom/display/libcopybit
+LOCAL_C_INCLUDES += hardware/qcom/display/libgenlock
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 ifeq ($(TARGET_HAVE_HDMI_OUT),true)
 LOCAL_CFLAGS += -DHDMI_DUAL_DISPLAY
