f54edbd924f1eabdc520eb60fbbb712fec7c7182
Rajulu Ponnada <rajulu@codeaurora.org>
2011-11-29 11:32:48 -0800

    hardware/qcom/display: support for QCOM tiled rendering
    Tiled rendering implementation.
    
    Change-Id: I1d1361b175fdb6f64b47df85b8f68bebd7d1eea5

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
 #Enables the listed display HAL modules
-display-hals := libhwcomposer liboverlay libgralloc libgenlock libcopybit
+display-hals := libhwcomposer liboverlay libgralloc libgenlock libcopybit libtilerenderer
 include $(call all-named-subdir-makefiles,$(display-hals))
 
---
+++ libtilerenderer/Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+ifeq ($(USE_OPENGL_RENDERER),true)
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_SHARED_LIBRARIES := libutils libcutils libGLESv2 libhwui
+
+LOCAL_C_INCLUDES += \
+	frameworks/base/include/utils \
+	frameworks/base/libs/hwui \
+	external/skia/include/core \
+	external/skia/include/effects \
+	external/skia/include/images \
+	external/skia/src/ports \
+	external/skia/include/utils \
+	hardware/libhardware/include/hardware \
+	frameworks/base/opengl/include/GLES2
+
+LOCAL_SRC_FILES := 	\
+	tilerenderer.cpp
+
+LOCAL_MODULE := libtilerenderer
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
+endif
