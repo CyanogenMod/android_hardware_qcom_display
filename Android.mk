dca703f7f1ab8ab60358922510507ff48bf0f1bd
freexperia <jerpelea@gmail.com>
2011-11-30 06:59:02 +0200

    msm: cleanup to compile msm7x30
    Change-Id: Ie1ca24c4f08de56a07c8e299ffbb00fc4af85a8a

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 #Enables the listed display HAL modules
 
-ifeq ($(call is-board-platform-in-list,$(MSM7K_BOARD_PLATFORMS)),true)
+#ifeq ($(call is-board-platform-in-list,$(MSM7K_BOARD_PLATFORMS)),true)
 	display-hals := libhwcomposer liboverlay libgralloc libcopybit
 	include $(call all-named-subdir-makefiles,$(display-hals))
-endif
+#endif
