9ecd41f95bb72bd4bdbeae5fd0fe22983ecf1911
Jeykumar Sankaran <jsanka@codeaurora.org>
2012-01-20 12:48:01 -0800

    Revert "Bypass: Turn off only after FB content is displayed."
    This reverts commit f2dbabe786a12e7e9ae15328550b5ba581c5492e.
    
    This change is not needed any more because we are caching the MDP pipe
    contents when closing the channel. So no need to wait for postFB finish.
    
    Change-Id: Iff5d8ab846ff09b6de24432d21f7e01635cb3db5

--- libgralloc/Android.mk
+++ libgralloc/Android.mk
@@ -43,8 +43,8 @@ ifeq ($(TARGET_QCOM_HDMI_OUT),true)
     LOCAL_SHARED_LIBRARIES += liboverlay
 endif
 
-ifeq ($(TARGET_HAVE_BYPASS),true)
-    LOCAL_CFLAGS += -DCOMPOSITION_BYPASS
+ifeq ($(TARGET_USES_SF_BYPASS),true)
+    LOCAL_CFLAGS += -DSF_BYPASS
 endif
 
 ifeq ($(TARGET_GRALLOC_USES_ASHMEM),true)
