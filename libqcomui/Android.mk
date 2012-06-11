202a77d28ac251545f6f998a974690212309b927
Iliyan Malchev <malchev@google.com>
2012-06-11 14:41:12 -0700

    initial GL libraries for msm8960
    Change-Id: I16451c70a079894ac326d3564d96f1fbafcd4f1b
    Signed-off-by: Iliyan Malchev <malchev@google.com>

---
+++ libqcomui/Android.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+        qcom_ui.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+        libutils \
+        libcutils \
+        libui \
+        libEGL \
+        libskia
+
+LOCAL_C_INCLUDES := $(TOP)/hardware/qcom/display/libgralloc \
+                    $(TOP)/frameworks/native/services/surfaceflinger \
+                    $(TOP)/external/skia/include/core \
+                    $(TOP)/external/skia/include/images
+
+LOCAL_CFLAGS := -DLOG_TAG=\"libQcomUI\"
+
+ifneq ($(call is-vendor-board-platform,QCOM),true)
+    LOCAL_CFLAGS += -DNON_QCOM_TARGET
+else
+    LOCAL_SHARED_LIBRARIES += libmemalloc
+endif
+
+LOCAL_CFLAGS += -DDEBUG_CALC_FPS
+
+LOCAL_MODULE := libQcomUI
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
