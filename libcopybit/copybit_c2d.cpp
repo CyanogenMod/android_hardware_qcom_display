b77bae74cbed4e19c4b975c280b4f8b1f6e51d61
Pawan Kumar <pavaku@codeaurora.org>
2012-06-25 11:33:49 +0530

    libcopybit: Fix UI blur with C2D composition
    Enable bi-linear filtering in c2d to fix UI blur
    with C2D composition.
    
    CRs-fixed: 367578
    (cherry picked from commit fb408361715418a32fc0d0f6379603f131898d26)
    
    Change-Id: Ie46f4fb40a4c6a9e10d31ad444894d4735e9d542

--- libcopybit/copybit_c2d.cpp
+++ libcopybit/copybit_c2d.cpp
@@ -1380,7 +1380,7 @@ static int open_copybit(const struct hw_module_t* module, const char* name,
     ctx->device.get = get;
     ctx->device.blit = blit_copybit;
     ctx->device.stretch = stretch_copybit;
-    ctx->blitState.config_mask = C2D_NO_BILINEAR_BIT | C2D_NO_ANTIALIASING_BIT;
+    ctx->blitState.config_mask = C2D_NO_ANTIALIASING_BIT;
     ctx->trg_transform = C2D_TARGET_ROTATE_0;
     ctx->g12_device_fd = open(G12_DEVICE_NAME, O_RDWR | O_SYNC);
     if (ctx->g12_device_fd < 0) {
