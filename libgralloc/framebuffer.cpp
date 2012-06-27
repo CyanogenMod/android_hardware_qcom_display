26145b899981b4776c627f3c5713f7f3bc910dd9
choongryeol.lee <choongryeol.lee@lge.com>
2012-06-26 23:41:00 -0700

    gralloc : apply kernel 3.4 fb screeninfo type
    Since fb screeninfo type is changed in kernel 3.4,
    fb ioctl is not work correctly without this patch.
    
    Change-Id: I5d673b78bd9a1680daf77256a7d31bc78d75cda7

--- libgralloc/framebuffer.cpp
+++ libgralloc/framebuffer.cpp
@@ -816,8 +816,8 @@ int mapFrameBufferLocked(struct private_module_t* module)
 
     float xdpi = (info.xres * 25.4f) / info.width;
     float ydpi = (info.yres * 25.4f) / info.height;
-    //The reserved[4] field is used to store FPS by the driver.
-    float fps  = info.reserved[4];
+    //The reserved[3] field is used to store FPS by the driver.
+    float fps  = info.reserved[3];
 
     ALOGI("using (fd=%d)\n"
           "id           = %s\n"
