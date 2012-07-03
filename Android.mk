b514889338b512bdb2550f93c5514af2f1580866
Sungmin Choi <sungmin.choi@lge.com>
2012-07-02 17:00:07 -0700

    add light hal module
    - initial version
    - control lcd backlight
    - control red, green, blue
    
    Change-Id: Ibd24dcee3a739111f9eb6a7717b76754875da36e

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
 
 display-hals := libqcomui
-display-hals += libgralloc libgenlock libcopybit libhwcomposer liboverlay
+display-hals += libgralloc libgenlock libcopybit libhwcomposer liboverlay liblight
 
 include $(call all-named-subdir-makefiles,$(display-hals))
 
