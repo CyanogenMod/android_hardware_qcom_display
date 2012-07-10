4a2207de17cb6eab7cec18b51fe49434bed54d71
Naseer Ahmed <naseer@codeaurora.org>
2012-07-10 01:42:20 -0700

    display: Add lights HAL
    Change-Id: If67d2180e395f60a8f7c458495b78b1cc456b847

--- Android.mk
+++ Android.mk
@@ -2,8 +2,8 @@
 #libs to be built for QCOM targets only
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
-display-hals := libgralloc libgenlock libcopybit
-display-hals +=libhwcomposer liboverlay libqdutils
+display-hals := libgralloc libgenlock libcopybit liblight
+display-hals += libhwcomposer liboverlay libqdutils
 endif
 
 include $(call all-named-subdir-makefiles,$(display-hals))
