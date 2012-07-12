5b805bf94234d872fecba54815c02ba8ea2aa784
Naseer Ahmed <naseer@codeaurora.org>
2012-07-11 23:43:42 -0700

    display: Clean up makefiles
    * Move common includes and shared libraries to
    a common location
    
    * Clean up Makefile copyrights
    
    Change-Id: I0427f0f98b4c039a691be0ef6f3deead2b4bfa02
    
    Conflicts:
    
    	libgralloc/Android.mk
    	libhwcomposer/Android.mk

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -1,23 +1,14 @@
 LOCAL_PATH := $(call my-dir)
+include $(LOCAL_PATH)/../common.mk
 include $(CLEAR_VARS)
-LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware libutils libdl \
-                          libEGL liboverlay libgenlock libqdutils \
-                          libhardware_legacy libmemalloc
-LOCAL_SRC_FILES :=  hwc.cpp          \
-                    hwc_video.cpp  \
-                    hwc_utils.cpp  \
-					hwc_uimirror.cpp \
-					hwc_ext_observer.cpp \
-					hwc_copybit.cpp
-LOCAL_MODULE := hwcomposer.$(TARGET_BOARD_PLATFORM)
-LOCAL_C_INCLUDES := hardware/qcom/display/libgralloc
-LOCAL_C_INCLUDES += hardware/qcom/display/libgenlock
-LOCAL_C_INCLUDES += hardware/qcom/display/liboverlay
-LOCAL_C_INCLUDES += hardware/qcom/display/libcopybit
-LOCAL_C_INCLUDES += hardware/qcom/display/libqdutils
-LOCAL_CFLAGS:= -DLOG_TAG=\"$(TARGET_BOARD_PLATFORM).hwcomposer\"
-
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE                  := hwcomposer.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_TAGS             := optional
+LOCAL_C_INCLUDES              := $(common_includes)
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay libgenlock \
+                                 libqdutils libhardware_legacy libdl libmemalloc
+LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"hwcomposer\"
+LOCAL_SRC_FILES               :=  hwc.cpp hwc_video.cpp hwc_utils.cpp \
+                                  hwc_uimirror.cpp hwc_ext_observer.cpp \
+                                  hwc_copybit.cpp
 include $(BUILD_SHARED_LIBRARY)
