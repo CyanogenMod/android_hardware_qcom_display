8f3e26887cf2507e42c641c74985ccb6002570fd
Naseer Ahmed <naseer@codeaurora.org>
2012-07-11 23:43:42 -0700

    display: Clean up makefiles
    * Move common includes and shared libraries to
    a common location
    
    * Clean up Makefile copyrights
    
    Change-Id: I0427f0f98b4c039a691be0ef6f3deead2b4bfa02
    
    Conflicts:
    
    	libgralloc/Android.mk
    	libhwcomposer/Android.mk

---
+++ common.mk
@@ -0,0 +1,24 @@
+#Common headers
+common_includes := hardware/qcom/display/libgralloc
+common_includes += hardware/qcom/display/libgenlock
+common_includes += hardware/qcom/display/liboverlay
+common_includes += hardware/qcom/display/libcopybit
+common_includes += hardware/qcom/display/libqdutils
+
+ifeq ($(TARGET_USES_POST_PROCESSING),true)
+    common_flags     += -DUSES_POST_PROCESSING
+    common_includes += $(TARGET_OUT_HEADERS)/pp/inc
+endif
+
+
+#Common libraries external to display HAL
+common_libs := liblog libutils libcutils libhardware
+
+#Common C flags
+common_flags := -DDEBUG_CALC_FPS -Wno-missing-field-initializers
+ifeq ($(TARGET_USES_ION),true)
+    common_flags += -DUSE_ION
+endif
+ifeq ($(ARCH_ARM_HAVE_NEON),true)
+    common_flags += -D__ARM_HAVE_NEON
+endif
