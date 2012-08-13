781769fa0bbaa701119dfb4a2d1052a5bbf1fd89
Arun Kumar K.R <akumarkr@codeaurora.org>
2012-08-13 13:22:49 -0700

    hwcomposer: Make file changes
    create separate lib for hwcexternal and ihwcservice
    
    Change-Id: Ie55b9e98eaf62d4aa2cb9f422a75f8195bfd6060

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -6,8 +6,8 @@ LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay libgenlock \
-                                 libqdutils libhardware_legacy libdl \
-                                 libmemalloc libbinder
+                                 libhwcexternal libqdutils libhardware_legacy \
+                                 libdl libmemalloc libhwcservice
 
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"hwcomposer\"
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
@@ -15,12 +15,38 @@ LOCAL_SRC_FILES               := hwc.cpp          \
                                  hwc_video.cpp    \
                                  hwc_utils.cpp    \
                                  hwc_uimirror.cpp \
-                                 hwc_external.cpp \
                                  hwc_uevents.cpp  \
                                  hwc_copybit.cpp  \
                                  hwc_mdpcomp.cpp  \
-                                 hwc_service.cpp  \
-                                 ihwc.cpp         \
                                  hwc_extonly.cpp
+
+include $(BUILD_SHARED_LIBRARY)
+
+#libhwcexternal library
+include $(CLEAR_VARS)
+LOCAL_MODULE                  := libhwcexternal
+LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_TAGS             := optional
+LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
+LOCAL_SHARED_LIBRARIES        := $(common_libs) liboverlay
+
+LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"hwcexternal\"
+LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_SRC_FILES               := hwc_external.cpp
+
+include $(BUILD_SHARED_LIBRARY)
+
+#libhwcservice library
+include $(CLEAR_VARS)
+LOCAL_MODULE                  := libhwcservice
+LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_TAGS             := optional
+LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libhwcexternal libbinder \
+
+LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"hwcservice\"
+LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_SRC_FILES               := hwc_service.cpp \
+                                 ihwc.cpp
 
 include $(BUILD_SHARED_LIBRARY)
