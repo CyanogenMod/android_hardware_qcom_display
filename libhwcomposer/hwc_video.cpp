ee2d82b39b40e7a331a58ca2110565dea0304c22
Arne Coucheron <arco68@gmail.com>
2012-10-13 00:33:28 +0200

    hwcomposer: 7x30: disable overlays for video playback
    check MDP revision and disable overlays for video playback usecase
    on 7x30.
    
    Change-Id: I8b179512b4aff136834dd80e4da7227a6b2aff4e

--- libhwcomposer/hwc_video.cpp
+++ libhwcomposer/hwc_video.cpp
@@ -20,6 +20,7 @@
 #include "hwc_qbuf.h"
 #include "hwc_video.h"
 #include "hwc_external.h"
+#include <mdp_version.h>
 
 namespace qhwc {
 
@@ -34,7 +35,9 @@ bool VideoOverlay::sIsModeOn = false;
 //Cache stats, figure out the state, config overlay
 bool VideoOverlay::prepare(hwc_context_t *ctx, hwc_layer_list_t *list) {
     sIsModeOn = false;
-    if(!ctx->mMDP.hasOverlay) {
+    if((!ctx->mMDP.hasOverlay) ||
+                            (qdutils::MDPVersion::getInstance().getMDPVersion()
+                             <= qdutils::MDP_V4_0)) {
        ALOGD_IF(VIDEO_DEBUG,"%s, this hw doesnt support overlay", __FUNCTION__);
        return false;
     }
