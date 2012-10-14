0171b876300beb2dec2c8d73a298b394e7bdcec8
Arnav Gupta <championswimmer@gmail.com>
2012-10-14 05:38:01 -0700

    Revert "hwcexternal: fixes in hwc_external"
    was causing build errors due to undeclared MSMFB_OVERLAY_COMMIT in most devices
    
    This reverts commit 4365be7f6c6d5758ed0cffe27dd38e589256164e.

--- libhwcomposer/hwc.cpp
+++ libhwcomposer/hwc.cpp
@@ -180,7 +180,6 @@ static int hwc_eventControl(struct hwc_composer_device* dev,
 
              if(ctx->mExtDisplay->isHDMIConfigured() &&
                 (ctx->mExtDisplay->getExternalDisplay()==EXTERN_DISPLAY_FB1)) {
-                // enableHDMIVsync will return -errno on error
                 ret = ctx->mExtDisplay->enableHDMIVsync(value);
              }
            break;
