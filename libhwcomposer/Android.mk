c5a2e9cf662600a7d0ca1c66651e7e22f498f8c2
Jerpelea <jerpelea@gmail.com>
2012-11-02 10:22:26 -0700

    Revert "hardware/qcom/display: Remove glfinish call"
    This reverts commit 2d69977f816031dacbf4378b2e54a3b6c4209d9a
    
    this commit breaks 11 sony devices msm7x30

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay libgenlock \
                                  libhwcexternal libqdutils libhardware_legacy \
-                                 libdl libmemalloc libhwcservice libGLESv1_CM
+                                 libdl libmemalloc libhwcservice
 
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"hwcomposer\"
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
