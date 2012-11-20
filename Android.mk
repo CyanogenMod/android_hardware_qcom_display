354278c3c2261e35d7fb916e40a78e14cc48a504
Shareef Ali <shareefalis@gmail.com>
2012-11-19 20:45:02 -0500

    qcom_display: add TARGET_PROVIDES_LIBLIGHT
    Change-Id: Ifa4984a3dc4b938fb89d6c65c8cda931d643f3c6

--- Android.mk
+++ Android.mk
@@ -1,7 +1,9 @@
 ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
 
-display-hals := libgralloc libgenlock libcopybit liblight
+display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
-
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+display-hals += liblight
+endif
 include $(call all-named-subdir-makefiles,$(display-hals))
 endif
