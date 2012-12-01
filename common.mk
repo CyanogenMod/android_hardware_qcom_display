7001595092e6fee28d627599a1a0093bcb857c5d
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2012-12-01 16:03:30 -0700

    display: allow disabling of HDMI out for devices which don't support it
    Change-Id: I23f07735b529f80d09663c35e79c6d810b837a16

--- common.mk
+++ common.mk
@@ -34,6 +34,10 @@ ifeq ($(TARGET_USES_ION), true)
     common_flags += -DUSE_ION
 endif
 
+ifeq ($(BOARD_HAS_NO_HDMI),true)
+    common_flags += -DNO_HDMI
+endif
+
 common_deps  :=
 kernel_includes :=
 
