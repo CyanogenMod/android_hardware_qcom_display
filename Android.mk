602b135bb098c26ec4f57fd081f16060d48e2f20
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2012-12-01 15:07:48 -0700

    qcom display: Bring back pmem/ashmem
    This brings back the pmem/ashmem memalloc implementations.
    
    Change-Id: I2bce129cc4dbe0f7fabf94895b6aa08ed7187b3f

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8974 msm8960 msm8660 msm7x27a msm7x27 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
@@ -6,4 +6,5 @@ ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
 endif
 include $(call all-named-subdir-makefiles,$(display-hals))
+
 endif
