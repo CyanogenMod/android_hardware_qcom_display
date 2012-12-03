94a45fdaf8a1b27d9f98a8c062bfe6c8b5643f27
Naseer Ahmed <naseer@codeaurora.org>
2012-12-03 15:53:36 -0500

    display: Fix includes
    Need to include from kernel headers and
    build for Qualcomm targets
    
    Change-Id: Ie77ba8c7dd129a9dc24e2f69b0ef768ea0580c3d

--- libgralloc/Android.mk
+++ libgralloc/Android.mk
@@ -20,20 +20,21 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := gralloc.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes)
+LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libmemalloc libgenlock
 LOCAL_SHARED_LIBRARIES        += libqdutils libGLESv1_CM
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"gralloc\"
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES               :=  gpu.cpp gralloc.cpp framebuffer.cpp mapper.cpp
 include $(BUILD_SHARED_LIBRARY)
 
 #MemAlloc Library
 include $(CLEAR_VARS)
-LOCAL_MODULE           := libmemalloc
-LOCAL_MODULE_TAGS      := optional
-LOCAL_C_INCLUDES       := $(common_includes)
-LOCAL_SHARED_LIBRARIES := $(common_libs) libgenlock
-LOCAL_CFLAGS           := $(common_flags) -DLOG_TAG=\"memalloc\"
-LOCAL_SRC_FILES        :=  ionalloc.cpp alloc_controller.cpp
+LOCAL_MODULE                  := libmemalloc
+LOCAL_MODULE_TAGS             := optional
+LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libgenlock
+LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"memalloc\"
+LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
+LOCAL_SRC_FILES               :=  ionalloc.cpp alloc_controller.cpp
 include $(BUILD_SHARED_LIBRARY)
