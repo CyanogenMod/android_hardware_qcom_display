4ca423e271248fb1d039365e4b2bce1ab05325f4
Naseer Ahmed <naseer@codeaurora.org>
2012-12-03 17:55:49 -0500

    display: Use fences for all targets
    Change-Id: I89a8b661dba7919707ba26ff35fb294b22aaba67

--- common.mk
+++ common.mk
@@ -21,12 +21,6 @@ common_libs := liblog libutils libcutils libhardware
 common_flags := -DDEBUG_CALC_FPS -Wno-missing-field-initializers
 common_flags += -Werror
 
-#TODO
-#ifeq ($(call is-vendor-board-platform,QCOM),true)
-ifeq ($(TARGET_BOARD_PLATFORM), msm8960)
-    common_flags += -DUSE_FENCE_SYNC
-endif
-
 ifeq ($(ARCH_ARM_HAVE_NEON),true)
     common_flags += -D__ARM_HAVE_NEON
 endif
