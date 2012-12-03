d48c074b5d9a5ff95f7cddc8a4b17de762bb3a5c
Naseer Ahmed <naseer@codeaurora.org>
2012-12-03 17:55:49 -0500

    display: Use fences for all targets
    Change-Id: I89a8b661dba7919707ba26ff35fb294b22aaba67

--- msm8084/libhwcomposer/hwc_utils.cpp
+++ msm8084/libhwcomposer/hwc_utils.cpp
@@ -239,7 +239,6 @@ bool isExternalActive(hwc_context_t* ctx) {
 
 int hwc_sync(hwc_context_t *ctx, hwc_display_contents_1_t* list, int dpy) {
     int ret = 0;
-#ifdef USE_FENCE_SYNC
     struct mdp_buf_sync data;
     int acquireFd[MAX_NUM_LAYERS];
     int count = 0;
@@ -280,7 +279,6 @@ int hwc_sync(hwc_context_t *ctx, hwc_display_contents_1_t* list, int dpy) {
         }
     }
     list->retireFenceFd = releaseFd;
-#endif
     return ret;
 }
 
