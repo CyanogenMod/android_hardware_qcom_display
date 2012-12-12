c6bd6d92c282c123915c2eb79163a58ad4a05bbb
Sushil Chauhan <sushilchauhan@codeaurora.org>
2012-12-12 12:33:01 -0800

    display: Add target specific flag to support Venus color format
    Venus color format is not supported on older platforms. Add target
    specific flag in display hal and define Venus macros to zero for
    older platforms.
    
    Change-Id: I38141ae870e255a0e54ce9fde46fe6cc3415f179

--- common.mk
+++ common.mk
@@ -25,6 +25,10 @@ ifeq ($(ARCH_ARM_HAVE_NEON),true)
     common_flags += -D__ARM_HAVE_NEON
 endif
 
+ifeq ($(TARGET_BOARD_PLATFORM), msm8974)
+    common_flags += -DVENUS_COLOR_FORMAT
+endif
+
 common_deps  :=
 kernel_includes :=
 
