3ba3431c8b63c3f198bb7b74395d5b6f01bc0a3a
Naseer Ahmed <naseer@codeaurora.org>
2012-12-13 13:09:53 -0500

    gralloc: Mark framebuffer as ION memory
    Change-Id: I859d4d03b62279b7e66ba52e3215fe7394a63434

--- msm8084/libgralloc/gpu.cpp
+++ msm8084/libgralloc/gpu.cpp
@@ -83,15 +83,14 @@ int gpu_context_t::gralloc_alloc_framebuffer_locked(size_t size, int usage,
         return -ENOMEM;
     }
 
-    // create a "fake" handles for it
-    // Set the PMEM flag as well, since adreno
-    // treats the FB memory as pmem
+    // create a "fake" handle for it
     intptr_t vaddr = intptr_t(m->framebuffer->base);
-    private_handle_t* hnd = new private_handle_t(dup(m->framebuffer->fd), bufferSize,
-                                                 private_handle_t::PRIV_FLAGS_USES_PMEM |
-                                                 private_handle_t::PRIV_FLAGS_FRAMEBUFFER,
-                                                 BUFFER_TYPE_UI, m->fbFormat, m->info.xres,
-                                                 m->info.yres);
+    private_handle_t* hnd = new private_handle_t(
+                                dup(m->framebuffer->fd), bufferSize,
+                                private_handle_t::PRIV_FLAGS_USES_ION |
+                                private_handle_t::PRIV_FLAGS_FRAMEBUFFER,
+                                BUFFER_TYPE_UI, m->fbFormat, m->info.xres,
+                                m->info.yres);
 
     // find a free slot
     for (uint32_t i=0 ; i<numBuffers ; i++) {
