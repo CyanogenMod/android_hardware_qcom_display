431ebb9fe68a42bf8d43e5edd89d6ad82a6d9247
Steve Kondik <shade@chemlab.org>
2012-12-20 20:48:54 -0800

    Don't build when variant is requested.
    Change-Id: I32adc414e5e68af6c409115583984a09d65b6587

--- Android.mk
+++ Android.mk
@@ -1,9 +1,10 @@
 ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
-
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),)
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
 endif
 include $(call all-named-subdir-makefiles,$(display-hals))
+endif
 endif
