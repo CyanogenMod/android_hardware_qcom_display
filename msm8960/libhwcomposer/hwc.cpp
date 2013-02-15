99cea4fc13cffa1f635c67bdafe72be9fee74086
Steve Kondik <shade@chemlab.org>
2013-02-14 18:05:18 -0800

    display: Add support for MDP 4.1
     * MDP 4.1 does not support the borderfill pipe and the baselayer logic
       should not be used.
     * Only use 3 pipes for MDP 4.1.
    
    Change-Id: Ide542079902f35818300619f8eefd3d188342b5d

--- msm8960/libhwcomposer/hwc.cpp
+++ msm8960/libhwcomposer/hwc.cpp
@@ -138,7 +138,8 @@ static int hwc_prepare_primary(hwc_composer_device_1 *dev,
         hwc_display_contents_1_t *list) {
     hwc_context_t* ctx = (hwc_context_t*)(dev);
     const int dpy = HWC_DISPLAY_PRIMARY;
-    if(UNLIKELY(!ctx->mBasePipeSetup))
+    if(UNLIKELY(!ctx->mBasePipeSetup) &&
+            qdutils::MDPVersion::getInstance().getMDPVersion() >= qdutils::MDP_V4_2)
         setupBasePipe(ctx);
     if (LIKELY(list && list->numHwLayers > 1) &&
             ctx->dpyAttr[dpy].isActive) {
