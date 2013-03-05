55290a90518c4a8e2cc41d7bdb40918248a9920e
Naseer Ahmed <naseer@codeaurora.org>
2013-03-05 11:34:14 -0500

    hwc: Check for correct screen state before enabling vsync
    Sometimes vsync would not be disabled before the display is
    blanked.
    This was due to the vsync disable ioctl being kicked off in a
    thread different from the event control which surfaceflinger
    called. So, while the calls from surfaceflinger are in order, the
    order of execution wasn't always the same. Hence, making sure
    ioctls are called in the same context to ensure order.
    
    Also
    - Make blanking/unblanking logs show the operation as a string.
    - Add a debug property to dump vsync timestamps (needs framework
    reboot)
    - Remove a log which showed delay in reading vsync. This log was
    unreliable when vsync is disabled and we're still reading from
    the sysfs node.
    
    Change-Id: Ibec04e9ffebd0ac6e1d32b7031e3668abd9390ff
    CRs-fixed: 443113

--- msm8084/libhwcomposer/hwc_utils.cpp
+++ msm8084/libhwcomposer/hwc_utils.cpp
@@ -98,6 +98,7 @@ void initContext(hwc_context_t *ctx)
     pthread_mutex_init(&(ctx->vstate.lock), NULL);
     pthread_cond_init(&(ctx->vstate.cond), NULL);
     ctx->vstate.enable = false;
+    ctx->vstate.fakevsync = false;
     ctx->mExtDispConfiguring = false;
 
     //Right now hwc starts the service but anybody could do it, or it could be
