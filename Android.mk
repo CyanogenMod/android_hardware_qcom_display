5b0a4a1080e095ef8c5f4cae5b982c75a455ee40
Naseer Ahmed <naseer@codeaurora.org>
2013-03-22 19:50:42 -0400

    display: Enable compilation for 8x74
    Change-Id: I8be8a7ab85ae721d2f5366982202cbc27a5f5d40

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
-ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8x74,$(TARGET_BOARD_PLATFORM)),)
     #This is for mako since it doesn't have the QCOM make functions
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
