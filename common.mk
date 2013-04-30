168582119a054183328c701afda7ed3775248d43
Shashank Mittal <mittals@codeaurora.org>
2013-04-30 14:25:56 -0700

    Enable support for Venus color format
    Change-Id: Ic0f530af44940e3507bcae6fe346b5f69f2cecb8

--- common.mk
+++ common.mk
@@ -28,11 +28,7 @@ ifeq ($(ARCH_ARM_HAVE_NEON),true)
 endif
 
 ifneq ($(filter msm8974 msm8x74 msm8226 msm8x26,$(TARGET_BOARD_PLATFORM)),)
-    # TODO: This define makes us pick a few inline functions
-    # from the kernel header media/msm_media_info.h. However,
-    # the bionic clean_headers utility scrubs them out.
-    # Figure out a way to import those macros correctly
-    # common_flags += -DVENUS_COLOR_FORMAT
+    common_flags += -DVENUS_COLOR_FORMAT
     common_flags += -DMDSS_TARGET
 endif
 
