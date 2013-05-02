8fd81c9e980152d1f54626f24b35e8950cff955e
Raj kamal <rkamal@codeaurora.org>
2013-05-02 17:28:23 +0530

    Fix crash issue arising out of multiple hdmi connect/disconnects
    Acquire mExtLock before proceeding further in
    hwc_prepare_external to prevent crashes arising out
    of updating ctx variables from two different
    threads(composition and uevent threads)
    
    Change-Id: I0f18c41b117f18aeb25572b53ef6eba3638bb826

--- libhwcomposer/hwc_utils.h
+++ libhwcomposer/hwc_utils.h
@@ -302,8 +302,8 @@ struct hwc_context_t {
     bool mSecureMode;
     //Lock to prevent set from being called while blanking
     mutable Locker mBlankLock;
-    //Lock to protect set when detaching external disp
-    mutable Locker mExtSetLock;
+    //Lock to protect prepare & set when detaching external disp
+    mutable Locker mExtLock;
     //DMA used for rotator
     bool mDMAInUse;
     //MDP rotater needed
