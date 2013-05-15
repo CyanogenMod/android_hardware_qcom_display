45563707a82ea20e3981e4522daa4f2ae16f3d4d
Naseer Ahmed <naseer@codeaurora.org>
2013-05-15 16:35:58 -0400

    hwc: Return an error in eventControl when blanked
    We cannot enable or disable vsync on a blanked display.
    
    Related-to-bug: 8968732
    Change-Id: Id3ec37128d527e3a512e1305bc0673a5789ac2d0
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- libhwcomposer/hwc.cpp
+++ libhwcomposer/hwc.cpp
@@ -246,6 +246,12 @@ static int hwc_eventControl(struct hwc_composer_device_1* dev, int dpy,
     int ret = 0;
     hwc_context_t* ctx = (hwc_context_t*)(dev);
     Locker::Autolock _l(ctx->mBlankLock);
+    if(!ctx->dpyAttr[dpy].isActive) {
+        ALOGE("Display is blanked - Cannot %s vsync",
+              enable ? "enable" : "disable");
+        return -EINVAL;
+    }
+
     switch(event) {
         case HWC_EVENT_VSYNC:
             if (ctx->vstate.enable == enable)
