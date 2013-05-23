b922a3848b54e5ab1c9fb9281c4197ef2f07b282
Saurabh Shah <saurshah@codeaurora.org>
2013-05-23 12:48:13 -0700

    hwc: In scaling check, consider out of bounds layers.
    In scaling check, consider out of bounds layers, apply trim
    and then decide if scaling is necessary or not.
    
    Change-Id: Iad7f662f42096b3f7a4a1c4cfe71048d275f4b5f

--- libhwcomposer/hwc_utils.cpp
+++ libhwcomposer/hwc_utils.cpp
@@ -319,15 +319,16 @@ void getAspectRatioPosition(hwc_context_t *ctx, int dpy, int orientation,
 }
 
 
-bool needsScaling(hwc_layer_1_t const* layer) {
+bool needsScaling(hwc_context_t* ctx, hwc_layer_1_t const* layer,
+        const int& dpy) {
     int dst_w, dst_h, src_w, src_h;
 
     hwc_rect_t displayFrame  = layer->displayFrame;
     hwc_rect_t sourceCrop = layer->sourceCrop;
+    trimLayer(ctx, dpy, layer->transform, sourceCrop, displayFrame);
 
     dst_w = displayFrame.right - displayFrame.left;
     dst_h = displayFrame.bottom - displayFrame.top;
-
     src_w = sourceCrop.right - sourceCrop.left;
     src_h = sourceCrop.bottom - sourceCrop.top;
 
@@ -337,8 +338,9 @@ bool needsScaling(hwc_layer_1_t const* layer) {
     return false;
 }
 
-bool isAlphaScaled(hwc_layer_1_t const* layer) {
-    if(needsScaling(layer) && isAlphaPresent(layer)) {
+bool isAlphaScaled(hwc_context_t* ctx, hwc_layer_1_t const* layer,
+        const int& dpy) {
+    if(needsScaling(ctx, layer, dpy) && isAlphaPresent(layer)) {
         return true;
     }
     return false;
@@ -415,7 +417,8 @@ void setListStats(hwc_context_t *ctx,
         if(layer->planeAlpha < 0xFF)
             ctx->listStats[dpy].planeAlpha = true;
         if(!ctx->listStats[dpy].needsAlphaScale)
-            ctx->listStats[dpy].needsAlphaScale = isAlphaScaled(layer);
+            ctx->listStats[dpy].needsAlphaScale =
+                    isAlphaScaled(ctx, layer, dpy);
 
         if(UNLIKELY(isExtOnly(hnd))){
             ctx->listStats[dpy].extOnlyLayerIndex = i;
