9f5ee653856435e82b561d2ff827ae3e48f4a0ca
Naseer Ahmed <naseer@codeaurora.org>
2013-06-25 17:17:04 -0400

    hwc: Get the retire fence from the MDP driver
    Get the retire fence separately instead of duplicating the
    release fence.
    
    Bug: 9319776
    Change-Id: Iacb773c2d94f635e27a7b355d399cb2e36dac2a5
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- common.mk
+++ common.mk
@@ -14,6 +14,10 @@ ifeq ($(TARGET_USES_POST_PROCESSING),true)
     common_includes += $(TARGET_OUT_HEADERS)/pp/inc
 endif
 
+ifeq ($(TARGET_DISPLAY_USE_RETIRE_FENCE),true)
+    common_flags += -DUSE_RETIRE_FENCE
+endif
+
 common_header_export_path := qcom/display
 
 #Common libraries external to display HAL
