76a5cdf91ae7c3f98845c0464dcc49c45d1af10b
Saurabh Shah <saurshah@codeaurora.org>
2013-06-27 10:03:19 -0700

    overlay: Add writeback support
    Add explicit writeback display type, writeback ioctls, memory mgmt,
    writeback class, writeback obj manager class to overlay.
    
    Change-Id: I171e60cea5ed8f6649859e3eb189df37b39962f1

--- msm8084/liboverlay/pipes/overlayGenPipe.h
+++ msm8084/liboverlay/pipes/overlayGenPipe.h
@@ -82,7 +82,7 @@ private:
     /* set Closed pipe */
     bool setClosed();
 
-    int mFbNum;
+    int mDpy;
     /* Ctrl/Data aggregator */
     CtrlData mCtrlData;
     //Whether we will do downscale opt. This is just a request. If the frame is
