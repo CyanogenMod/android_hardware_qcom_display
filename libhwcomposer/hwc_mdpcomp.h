088bd89d64f943879eabe845c4d866586e7fb1bc
Prabhanjan Kandula <pkandula@codeaurora.org>
2013-07-02 23:47:13 +0530

    hwc : Fix crash during dumpsys
    Dumpsys runs in different thread not in composition thread.
    Accessing mdp composition frame info in dumpsys with out
    synchronizing could cause crash since frame info can get reset
    in composition thread.
    
    CRs-fixed: 503268
    
    Change-Id: If3f7d8cc9fce8b14622e666fa52583ff0cfdedd2

--- libhwcomposer/hwc_mdpcomp.h
+++ libhwcomposer/hwc_mdpcomp.h
@@ -173,6 +173,7 @@ protected:
     static IdleInvalidator *idleInvalidator;
     struct FrameInfo mCurrentFrame;
     struct LayerCache mCachedFrame;
+    mutable Locker mMdpCompLock;
 };
 
 class MDPCompLowRes : public MDPComp {
