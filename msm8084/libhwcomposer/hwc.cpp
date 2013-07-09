6af3757806fc72028ea15f27db1a4e0fd439dfd9
Naseer Ahmed <naseer@codeaurora.org>
2013-07-09 16:36:28 -0400

    hwc: Don't lock eventControl for vsync events
    Locking here can lead to a delay in vsync disable/enable
    where prepare/set is not yet completed. The HWC spec says that
    eventControl needs to take effect immediately.
    This lock was earlier present when we waited for SF to unblank
    primary at startup and the vsync control ioctls could fail.
    This is no longer necessary as we unblank primary on startup now.
    
    Change-Id: I2bf18ae9235f8dd428a495a9a23af7f00f34ff1d

--- msm8084/libhwcomposer/hwc.cpp
+++ msm8084/libhwcomposer/hwc.cpp
@@ -242,7 +242,6 @@ static int hwc_eventControl(struct hwc_composer_device_1* dev, int dpy,
 {
     int ret = 0;
     hwc_context_t* ctx = (hwc_context_t*)(dev);
-    Locker::Autolock _l(ctx->mBlankLock);
     switch(event) {
         case HWC_EVENT_VSYNC:
             if (ctx->vstate.enable == enable)
@@ -256,6 +255,7 @@ static int hwc_eventControl(struct hwc_composer_device_1* dev, int dpy,
 #ifdef QCOM_BSP
         case  HWC_EVENT_ORIENTATION:
             if(dpy == HWC_DISPLAY_PRIMARY) {
+                Locker::Autolock _l(ctx->mBlankLock);
                 // store the primary display orientation
                 // will be used in hwc_video::configure to disable
                 // rotation animation on external display
