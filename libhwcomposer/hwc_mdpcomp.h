c86b27706c234e0b77d706e0f8cbbeab231c4035
Saurabh Shah <saurshah@codeaurora.org>
2013-07-22 16:33:23 -0700

    hwc: Configure framebuffer before other layers.
    If framebuffer needs to be configured, configure it before all the
    other layers in mdp composition. This helps in cases where we are
    out of SMPs and framebuffer configuration fails owing to that.
    Framebuffer being the fallback path, should always get highest
    priority when reserving SMP blocks
    
    Change-Id: Ie7a6903d1b9fb98b308689c81522571449bf2e8e

--- libhwcomposer/hwc_mdpcomp.h
+++ libhwcomposer/hwc_mdpcomp.h
@@ -163,6 +163,7 @@ protected:
     void updateYUV(hwc_context_t* ctx, hwc_display_contents_1_t* list);
     bool programMDP(hwc_context_t *ctx, hwc_display_contents_1_t* list);
     bool programYUV(hwc_context_t *ctx, hwc_display_contents_1_t* list);
+    void reset(const int& numAppLayers, hwc_display_contents_1_t* list);
 
     int mDpy;
     static bool sEnabled;
@@ -226,5 +227,6 @@ private:
 
     virtual int pipesNeeded(hwc_context_t *ctx, hwc_display_contents_1_t* list);
 };
+
 }; //namespace
 #endif
