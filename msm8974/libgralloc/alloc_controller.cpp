32c2c1b1490e949a15dca9351f213d91be2b79d5
Naseer Ahmed <naseer@codeaurora.org>
2013-07-26 20:14:41 -0400

    gralloc: Add allocation support for sRGB formats
    Refer to graphics.h for the definition of these new formats.
    The HWC doesn't yet support output to these formats.
    
    Change-Id: Ib924e7b41ccedb808631e0a23f706f3d9a209048

--- msm8974/libgralloc/alloc_controller.cpp
+++ msm8974/libgralloc/alloc_controller.cpp
@@ -103,7 +103,7 @@ int AdrenoMemInfo::getStride(int width, int format)
 {
     int stride = ALIGN(width, 32);
     // Currently surface padding is only computed for RGB* surfaces.
-    if (format < 0x7) {
+    if (format <= HAL_PIXEL_FORMAT_sRGB_888) {
         // Don't add any additional padding if debug.gralloc.map_fb_memory
         // is enabled
         char property[PROPERTY_VALUE_MAX];
@@ -117,6 +117,7 @@ int AdrenoMemInfo::getStride(int width, int format)
         switch(format)
         {
             case HAL_PIXEL_FORMAT_RGB_888:
+            case HAL_PIXEL_FORMAT_sRGB_888:
                 bpp = 3;
                 break;
             case HAL_PIXEL_FORMAT_RGB_565:
@@ -283,9 +284,11 @@ size_t getBufferSizeAndDimensions(int width, int height, int format,
         case HAL_PIXEL_FORMAT_RGBA_8888:
         case HAL_PIXEL_FORMAT_RGBX_8888:
         case HAL_PIXEL_FORMAT_BGRA_8888:
+        case HAL_PIXEL_FORMAT_sRGB_A_8888:
             size = alignedw * alignedh * 4;
             break;
         case HAL_PIXEL_FORMAT_RGB_888:
+        case HAL_PIXEL_FORMAT_sRGB_888:
             size = alignedw * alignedh * 3;
             break;
         case HAL_PIXEL_FORMAT_RGB_565:
