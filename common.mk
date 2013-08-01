e907c49d19362c3b66815694696cb514d352cdc3
Steve Kondik <shade@chemlab.org>
2013-07-31 17:26:40 -0700

    display: Fix handling of QCOM_BSP
     * Only set this CFLAG if the device declares TARGET_USES_QCOM_BSP.
    
    Change-Id: Id1c02c5d1cca9016d46005333ba1e2a22e75e1cb

--- common.mk
+++ common.mk
@@ -30,13 +30,10 @@ common_deps  :=
 kernel_includes :=
 
 # Executed only on QCOM BSPs
-# Explicitly exclude mako, since CM adds the QCOM make functions and triggers
-# this block incorrectly
-ifeq ($(TARGET_QCOM_OLD_BSP),)
-ifeq ($(filter mako occam,$(TARGET_DEVICE)),)
+ifeq ($(TARGET_USES_QCOM_BSP),true)
     common_flags += -DQCOM_BSP
 endif
-endif
+
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
