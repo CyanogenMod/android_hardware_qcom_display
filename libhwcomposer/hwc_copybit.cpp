46cf4e9ddc46e753c6e6b01e3d1ab1c84328da8b
Radhika Ranjan Soni <rrsoni@codeaurora.org>
2013-08-06 16:40:05 +0530

    hwc/copybit: Use gpu composition for plane alpha
    MDP3 doesn't support plane alpha. So falling back to
    gpu whenever there is plane alpha in the list of layers.
    
    Change-Id: Iec40e5160f4320b7393d937444645d1a243bd83b

--- libhwcomposer/hwc_copybit.cpp
+++ libhwcomposer/hwc_copybit.cpp
@@ -166,7 +166,10 @@ bool CopyBit::prepare(hwc_context_t *ctx, hwc_display_contents_1_t *list,
     private_handle_t *fbHnd = (private_handle_t *)fbLayer->handle;
 
 
-    // Avoid issue with mdp3 and rotation by using gpu composition
+    // Following are MDP3 limitations for which we
+    // need to fallback to GPU composition:
+    // 1. HW issues with mdp3 and rotation.
+    // 2. Plane alpha is not supported by MDP3.
     if (qdutils::MDPVersion::getInstance().getMDPVersion() < 400) {
         for (int i = ctx->listStats[dpy].numAppLayers-1; i >= 0 ; i--) {
             hwc_layer_1_t *layer = (hwc_layer_1_t *) &list->hwLayers[i];
@@ -174,6 +177,8 @@ bool CopyBit::prepare(hwc_context_t *ctx, hwc_display_contents_1_t *list,
                    HAL_TRANSFORM_FLIP_V | HAL_TRANSFORM_ROT_90)) &&
                    ((layer->displayFrame.bottom - layer->displayFrame.top) % 16 ||
                    (layer->displayFrame.right - layer->displayFrame.left) % 16))
+                return true;
+            if (layer->planeAlpha != 0xFF)
                 return true;
         }
     }
