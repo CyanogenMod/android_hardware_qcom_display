5a74377f05354815ceb696c1ecc1b68d3d16fa72
Saurabh Shah <saurshah@codeaurora.org>
2013-08-16 16:10:01 -0700

    hwc: Bump up version to 1.3
    Increment hwc version to 1.3.
    Create and use a converter from float source crop to integers.
    
    Change-Id: I5e185195c975b3a9434c5d9dce9ae4757ff98739

--- libhwcomposer/hwc_copybit.cpp
+++ libhwcomposer/hwc_copybit.cpp
@@ -325,7 +325,7 @@ int  CopyBit::drawLayerUsingCopybit(hwc_context_t *dev, hwc_layer_1_t *layer,
     src.vert_padding = 0;
 
     // Copybit source rect
-    hwc_rect_t sourceCrop = layer->sourceCrop;
+    hwc_rect_t sourceCrop = integerizeSourceCrop(layer->sourceCropf);
     copybit_rect_t srcRect = {sourceCrop.left, sourceCrop.top,
                               sourceCrop.right,
                               sourceCrop.bottom};
