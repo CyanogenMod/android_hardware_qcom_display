e1b6b91bcf3c9559d14cae2bfc2b00c4e7c6a5a5
Ajay Dudani <adudani@codeaurora.org>
2013-08-19 18:53:47 -0700

    Add msm8226 support for display hal
    Update code path to use msm8974 tree for msm8226 as they
    share common code source.
    
    Change-Id: I92584de50b43120a6ed638fdb4482ff732f0ef9c

--- Android.mk
+++ Android.mk
@@ -1,13 +1,8 @@
-ifneq ($(filter msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
-    #This is for 8974 based platforms
+ifneq ($(filter msm8974 msm8x74 msm8226,$(TARGET_BOARD_PLATFORM)),)
+    #This is for 8974 based (and B-family) platforms
     include $(call all-named-subdir-makefiles,msm8974)
-else
-ifneq ($(filter msm8226,$(TARGET_BOARD_PLATFORM)),)
-    #This is for 8226 based platforms
-    include $(call all-named-subdir-makefiles,msm8x26)
 else
     #This is for 8960 based platforms
     include $(call all-named-subdir-makefiles,msm8960)
-endif
 endif
 
