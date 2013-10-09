ae3e45776c9806c38bdb35c32a140bb00457a5ad
Terence Hampson <thampson@codeaurora.org>
2013-10-09 11:32:21 -0400

    hwc: Remove need for intermediate buffer for c2d composition
    A-Family device NON PRO 8960 uses copybit composition. The
    intermediate buffers allocated are not needed when we can
    compose onto fb frame.
    
    Change-Id: I13fd2deb328a3ef4b6738cfe7030fe67ea7bd6c6
    
    Conflicts:
            libhwcomposer/hwc_copybit.cpp
            libhwcomposer/hwc_utils.cpp

--- libhwcomposer/hwc_copybit.cpp
+++ libhwcomposer/hwc_copybit.cpp
@@ -23,6 +23,7 @@
 #include <utils/Timers.h>
 #include "hwc_copybit.h"
 #include "comptype.h"
+#include "mdp_version.h"
 #include "gr.h"
 
 namespace qhwc {
@@ -172,7 +173,8 @@ bool CopyBit::prepare(hwc_context_t *ctx, hwc_display_contents_1_t *list,
 
 
     //Allocate render buffers if they're not allocated
-    if (useCopybitForYUV || useCopybitForRGB) {
+    if (ctx->mMDP.version > qdutils::MDP_V4_3 &&
+            (useCopybitForYUV || useCopybitForRGB)) {
         int ret = allocRenderBuffers(mAlignedFBWidth,
                                      mAlignedFBHeight,
                                      HAL_PIXEL_FORMAT_RGBA_8888);
@@ -192,7 +194,10 @@ bool CopyBit::prepare(hwc_context_t *ctx, hwc_display_contents_1_t *list,
         if ((hnd->bufferType == BUFFER_TYPE_VIDEO && useCopybitForYUV) ||
             (hnd->bufferType == BUFFER_TYPE_UI && useCopybitForRGB)) {
             layerProp[i].mFlags |= HWC_COPYBIT;
-            list->hwLayers[i].compositionType = HWC_OVERLAY;
+            if (ctx->mMDP.version <= qdutils::MDP_V4_3)
+                list->hwLayers[i].compositionType = HWC_BLIT;
+            else
+                list->hwLayers[i].compositionType = HWC_OVERLAY;
             mCopyBitDraw = true;
         } else {
             // We currently cannot mix copybit layers with layers marked to
@@ -229,23 +234,38 @@ bool CopyBit::draw(hwc_context_t *ctx, hwc_display_contents_1_t *list,
     // draw layers marked for COPYBIT
     int retVal = true;
     int copybitLayerCount = 0;
+    uint32_t last = 0;
     LayerProp *layerProp = ctx->layerProp[dpy];
+    private_handle_t *renderBuffer;
 
     if(mCopyBitDraw == false) // there is no layer marked for copybit
         return false ;
 
     //render buffer
-    private_handle_t *renderBuffer = getCurrentRenderBuffer();
+    if (ctx->mMDP.version <= qdutils::MDP_V4_3) {
+        last = list->numHwLayers - 1;
+        renderBuffer = (private_handle_t *)list->hwLayers[last].handle;
+    } else {
+        renderBuffer = getCurrentRenderBuffer();
+    }
     if (!renderBuffer) {
         ALOGE("%s: Render buffer layer handle is NULL", __FUNCTION__);
         return false;
     }
 
-    //Wait for the previous frame to complete before rendering onto it
-    if(mRelFd[mCurRenderBufferIndex] >=0) {
-        sync_wait(mRelFd[mCurRenderBufferIndex], 1000);
-        close(mRelFd[mCurRenderBufferIndex]);
-        mRelFd[mCurRenderBufferIndex] = -1;
+    if (ctx->mMDP.version > qdutils::MDP_V4_3) {
+        //Wait for the previous frame to complete before rendering onto it
+        if(mRelFd[mCurRenderBufferIndex] >=0) {
+            sync_wait(mRelFd[mCurRenderBufferIndex], 1000);
+            close(mRelFd[mCurRenderBufferIndex]);
+            mRelFd[mCurRenderBufferIndex] = -1;
+        }
+    } else {
+        if(list->hwLayers[last].acquireFenceFd >=0) {
+            sync_wait(list->hwLayers[last].acquireFenceFd, 1000);
+            close(list->hwLayers[last].acquireFenceFd);
+            list->hwLayers[last].acquireFenceFd = -1;
+        }
     }
 
     //Clear the visible region on the render buffer
