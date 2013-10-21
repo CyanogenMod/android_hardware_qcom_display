312eebfcdfbc65b5c1c3c0f3c881254e1b1722f1
radhakrishna <radhakrishna@codeaurora.org>
2013-10-21 16:49:21 +0530

    display : Add wormholeRegion Calculation
    For copybit cases we should clear wormhole region
    rather than nonwormhole. Adding support to calculate
    and clear the same
    
    Conflicts:
        libqdutils/Android.mk
    
    CRs-Fixed: 624456
    Change-Id: Ie85d1b484174199e35f43ce95b30de1fac9c30b2

--- libqdutils/Android.mk
+++ libqdutils/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE                  := libqdutils
 LOCAL_MODULE_TAGS             := optional
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libbinder libqservice
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libbinder libqservice libui
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdutils\"
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
@@ -12,7 +12,8 @@ LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
 LOCAL_COPY_HEADERS            := display_config.h mdp_version.h
 LOCAL_SRC_FILES               := profiler.cpp mdp_version.cpp \
                                  idle_invalidator.cpp \
-                                 comptype.cpp display_config.cpp
+                                 comptype.cpp display_config.cpp \
+                                 cb_utils.cpp
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
