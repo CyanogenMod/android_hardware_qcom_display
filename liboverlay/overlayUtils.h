c9d00cd1e8a2a60f56679fbadd0ae6c13d552201
Zohaib Alam <zalam@codeaurora.org>
2014-01-24 16:38:32 -0500

    hwc: vpu: Added a new flag to indicate VPU pipe
    Added a new layer flag: HWC_VPU_PIPE to indicate that the layer
    will be drawn by VPU. There is a corresponding MDP pipe
    flag: MDP_VPU_PIPE, which should be set for the VPU layers to
    indicate the layer will be drawn by VPU, so that MDP driver does
    the appropriate settings to allow this process.
    
    Change-Id: I61270aca633bb831bf34d413877bf925a3d2ac37

--- liboverlay/overlayUtils.h
+++ liboverlay/overlayUtils.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
+* Copyright (c) 2011-2014, The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
@@ -272,6 +272,7 @@ enum eMdpFlags {
     OV_MDSS_MDP_BWC_EN = MDP_BWC_EN,
     OV_MDSS_MDP_DUAL_PIPE = MDSS_MDP_DUAL_PIPE,
     OV_MDP_SOLID_FILL = MDP_SOLID_FILL,
+    OV_MDP_VPU_PIPE = MDP_VPU_PIPE,
 };
 
 enum eZorder {
