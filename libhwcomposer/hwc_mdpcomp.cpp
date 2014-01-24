c2f04cf755b91cb373da3bc2f7faec8b9272fc76
Saurabh Shah <saurshah@codeaurora.org>
2014-01-23 18:39:01 -0800

    qdutils: idle_invalidator: Use fixed timeout rather than range
    Currently the idle timeout is in a range from idle_timeout / 2
    to idle_timeout, since we didn't need precise values.
    
    Change this to a fixed value using timestamps, because the timeout
    is too low and warrants precision.
    
    Change-Id: I9cec1db7f1d7fda2996ab80449c61269915f6be4

--- libhwcomposer/hwc_mdpcomp.cpp
+++ libhwcomposer/hwc_mdpcomp.cpp
@@ -1600,7 +1600,7 @@ bool MDPCompNonSplit::draw(hwc_context_t *ctx, hwc_display_contents_1_t* list) {
 
     /* reset Invalidator */
     if(idleInvalidator && !sIdleFallBack && mCurrentFrame.mdpCount)
-        idleInvalidator->markForSleep();
+        idleInvalidator->handleUpdateEvent();
 
     overlay::Overlay& ov = *ctx->mOverlay;
     LayerProp *layerProp = ctx->layerProp[mDpy];
@@ -1947,7 +1947,7 @@ bool MDPCompSplit::draw(hwc_context_t *ctx, hwc_display_contents_1_t* list) {
 
     /* reset Invalidator */
     if(idleInvalidator && !sIdleFallBack && mCurrentFrame.mdpCount)
-        idleInvalidator->markForSleep();
+        idleInvalidator->handleUpdateEvent();
 
     overlay::Overlay& ov = *ctx->mOverlay;
     LayerProp *layerProp = ctx->layerProp[mDpy];
