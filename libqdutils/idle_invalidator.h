92abb4f2adab04268f44a4948b4354be7b6d88f7
Ramkumar Radhakrishnan <ramkumar@codeaurora.org>
2014-02-06 21:31:29 -0800

    qdutils: Refactor idletimeout fallback mechanism.
    1. Use kernel timer interrupt to signal about the timeout to
       Invalidator thread and fallback to GPU composition.
    2. This implementation avoids waking up the Invalidator thread for
       every idletimeout value during continuous update.
    
    Change-Id: I4370f10a0ca06b5cb1e7bdcb047e9c8daec51afb

--- libqdutils/idle_invalidator.h
+++ libqdutils/idle_invalidator.h
@@ -38,19 +38,15 @@ typedef void (*InvalidatorHandler)(void*);
 
 class IdleInvalidator : public android::Thread {
     void *mHwcContext;
-    struct timeval mLastUpdateTime;
-    bool mSleepAgain;
-    unsigned int mSleepTime;
+    int mTimeoutEventFd;
     static InvalidatorHandler mHandler;
     static android::sp<IdleInvalidator> sInstance;
-    mutable Locker mLock;
 
     public:
     IdleInvalidator();
     /* init timer obj */
     int init(InvalidatorHandler reg_handler, void* user_data, unsigned int
              idleSleepTime);
-    void handleUpdateEvent();
     /*Overrides*/
     virtual bool        threadLoop();
     virtual int         readyToRun();
