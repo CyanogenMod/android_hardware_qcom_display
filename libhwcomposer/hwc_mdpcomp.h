c2d78d8f2c24b933602f5e363c653e7497e6d841
Jeykumar Sankaran <jsanka@codeaurora.org>
2014-02-14 14:55:29 -0800

    h/q/d: Optimize framebuffer layer update
    While doing mixed mode composition, update only the region of the
    framebuffer where layer contents are cached or updated. Avoids
    MDP fetch for non-visible contents of FB.
    
    Change-Id: I20997d79e02a6bac60d7333c28c6dde134263197

--- libhwcomposer/hwc_mdpcomp.h
+++ libhwcomposer/hwc_mdpcomp.h
@@ -223,6 +223,8 @@ protected:
     void reset(hwc_context_t *ctx);
     bool isSupportedForMDPComp(hwc_context_t *ctx, hwc_layer_1_t* layer);
     bool resourceCheck(hwc_context_t *ctx, hwc_display_contents_1_t *list);
+    hwc_rect_t getUpdatingFBRect(hwc_context_t *ctx,
+            hwc_display_contents_1_t* list);
 
     int mDpy;
     static bool sEnabled;
