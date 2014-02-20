998b424d791922eee6b8535f8920699719eff4f2
Manoj Kumar AVM <manojavm@codeaurora.org>
2014-02-19 18:53:14 -0800

    gralloc: Add GRALLOC_PRIVATE_USAGE_WFD flag
    1. Screenrecord and WFD use virtual display and encoder.
       Since HAL supports only one virtual display, we need a
       definite way to distinguish both use-cases.
    
       Failure to differentiate will break Screen record + WFD
       concurrent usecase.
    
       WFD stack will set this usage flag and SF will query the usage
       flags to differentiate WFD and screenrecord usecase.
    
    2. Deprecate GRALLOC_USAGE_PRIVATE_EXTERNAL_CC and
       GRALLOC_USAGE_PRIVATE_EXTERNAL_BLOCK flags as these are no
       longer used.
    
    Change-Id: Ifa260c5af2371eea830744ce62a5a83b8cfd0de7

--- libgralloc/gralloc_priv.h
+++ libgralloc/gralloc_priv.h
@@ -60,13 +60,8 @@ enum {
     /* Buffer content should be displayed on an external display only */
     GRALLOC_USAGE_PRIVATE_EXTERNAL_ONLY   =       0x08000000,
 
-    /* Only this buffer content should be displayed on external, even if
-     * other EXTERNAL_ONLY buffers are available. Used during suspend.
-     */
-    GRALLOC_USAGE_PRIVATE_EXTERNAL_BLOCK  =       0x00100000,
-
-    /* Close Caption displayed on an external display only */
-    GRALLOC_USAGE_PRIVATE_EXTERNAL_CC     =       0x00200000,
+    /* This flag is set for WFD usecase */
+    GRALLOC_USAGE_PRIVATE_WFD             =       0x00200000,
 
     /* CAMERA heap is a carveout heap for camera, is not secured*/
     GRALLOC_USAGE_PRIVATE_CAMERA_HEAP     =       0x00400000,
@@ -160,10 +155,6 @@ struct private_handle_t : public native_handle {
             PRIV_FLAGS_NOT_MAPPED         = 0x00001000,
             // Display on external only
             PRIV_FLAGS_EXTERNAL_ONLY      = 0x00002000,
-            // Display only this buffer on external
-            PRIV_FLAGS_EXTERNAL_BLOCK     = 0x00004000,
-            // Display this buffer on external as close caption
-            PRIV_FLAGS_EXTERNAL_CC        = 0x00008000,
             PRIV_FLAGS_VIDEO_ENCODER      = 0x00010000,
             PRIV_FLAGS_CAMERA_WRITE       = 0x00020000,
             PRIV_FLAGS_CAMERA_READ        = 0x00040000,
