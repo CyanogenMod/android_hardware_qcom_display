d11c498a1a4e64a589e37f4b6e6cada9e256b163
Saurabh Shah <saurshah@codeaurora.org>
2014-03-28 16:33:13 -0700

    hwc: featurize using QCOM_BSP
    - Use QCOM_BSP for HWC_BLIT and GPUHint features
    - These are not avaliable on all platforms
    
    Change-Id: Ied0c60246093c868ac708f2f8897f08cec1e8878

--- libhwcomposer/hwc_copybit.cpp
+++ libhwcomposer/hwc_copybit.cpp
@@ -27,6 +27,8 @@
 #include "cb_utils.h"
 #include "cb_swap_rect.h"
 #include "math.h"
+#include "sync/sync.h"
+
 using namespace qdutils;
 namespace qhwc {
 
@@ -230,9 +232,11 @@ bool CopyBit::prepare(hwc_context_t *ctx, hwc_display_contents_1_t *list,
         // Mark all layers to be drawn by copybit
         for (int i = ctx->listStats[dpy].numAppLayers-1; i >= 0 ; i--) {
             layerProp[i].mFlags |= HWC_COPYBIT;
+#ifdef QCOM_BSP
             if (ctx->mMDP.version == qdutils::MDP_V3_0_4)
                 list->hwLayers[i].compositionType = HWC_BLIT;
             else
+#endif
                 list->hwLayers[i].compositionType = HWC_OVERLAY;
         }
     }
