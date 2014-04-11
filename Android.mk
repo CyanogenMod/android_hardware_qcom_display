07bded6ac921e7049cc4b5075c0f9cd5a402ea4b
Naseer Ahmed <naseer@codeaurora.org>
2014-04-11 19:46:04 -0400

    display: Makefile changes for AOSP builds
    - On some builds, the display HAL can be located in a subdirectory.
    Use LOCAL_PATH to include local headers.
    - Use msm and apq filters to pick up the qcom display HAL.
    
    Change-Id: Ib9b47ececb37fda1ecde5ce1cbe1fce9b041f84a

--- Android.mk
+++ Android.mk
@@ -4,8 +4,7 @@ display-hals += libmemtrack
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
-ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
-    #This is for mako since it doesn't have the QCOM make functions
+ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
 endif
