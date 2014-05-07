f0336ae3e21ef0eb26231ab4093ee6eaaad0e14c
Vineeta Srivastava <vsrivastava@google.com>
2014-05-07 16:27:25 +0000

    Revert "Revert "copybit: Export c2d2 headers from display HAL""
    This reverts commit 45035563c19025b6acd4f9a30c8e3f5c36192aab.
    
    Change-Id: I895337687d75993b701e0f09dd44cf6bf98ea007

--- msm8960/libcopybit/Android.mk
+++ msm8960/libcopybit/Android.mk
@@ -17,7 +17,7 @@ include $(LOCAL_PATH)/../common.mk
 include $(CLEAR_VARS)
 
 LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
-LOCAL_COPY_HEADERS            := copybit.h copybit_priv.h
+LOCAL_COPY_HEADERS            := copybit.h copybit_priv.h c2d2.h
 #Copy the headers regardless of whether copybit is built
 include $(BUILD_COPY_HEADERS)
 
