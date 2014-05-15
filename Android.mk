11bcd0d009f05e6bb39a308a0e3de30fa56ba7f8
Naseer Ahmed <naseer@codeaurora.org>
2014-05-14 20:43:49 -0400

    display: Compile HALs for 8084
    Change-Id: Ia930317ad0c5a2f68fb427a2eef12b66658ae1e7

--- Android.mk
+++ Android.mk
@@ -1,3 +1,7 @@
+ifneq ($(filter msm8084 msm8x84,$(TARGET_BOARD_PLATFORM)),)
+    #This is for 8084 based platforms
+    include $(call all-named-subdir-makefiles,msm8084)
+else
 ifneq ($(filter msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
     #This is for 8974 based (and B-family) platforms
     include $(call all-named-subdir-makefiles,msm8974)
@@ -7,5 +11,6 @@ ifneq ($(filter msm8226 msm8x26,$(TARGET_BOARD_PLATFORM)),)
 else
     #This is for 8960 based platforms
     include $(call all-named-subdir-makefiles,msm8960)
+endif
 endif
 endif
