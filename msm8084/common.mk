83c416f42423e83f233664c5dc28dcae5c5ad785
Naseer Ahmed <naseer@codeaurora.org>
2014-05-28 18:33:32 -0400

    display: Use msm8084 instead of apq8084
    Bug: 15116418
    
    Change-Id: I15bcc9b96f7d74c950c201beb9ba62489666f0cc

--- msm8084/common.mk
+++ msm8084/common.mk
@@ -27,7 +27,7 @@ ifeq ($(ARCH_ARM_HAVE_NEON),true)
     common_flags += -D__ARM_HAVE_NEON
 endif
 
-ifeq ($(call is-board-platform-in-list, msm8974 msm8226 msm8610 apq8084 \
+ifeq ($(call is-board-platform-in-list, msm8974 msm8226 msm8610 msm8084 \
         mpq8092 msm_bronze msm8916), true)
     common_flags += -DVENUS_COLOR_FORMAT
     common_flags += -DMDSS_TARGET
