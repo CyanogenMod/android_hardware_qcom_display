0415b73fdf38f845a91ed9e971519edb92b5cbf8
Naseer Ahmed <naseer@codeaurora.org>
2014-05-29 16:36:34 -0400

    gralloc: Align NV21 to 32 pixels
    Graphics requires 32 pixel alignment and camera driver is being
    updated to this.
    
    Change-Id: I00fe659f7a16abeb8f65d9bbf41a24a7c8113bfb
    CRs-fixed: 476475

--- libgralloc/alloc_controller.cpp
+++ libgralloc/alloc_controller.cpp
@@ -181,6 +181,7 @@ void AdrenoMemInfo::getAlignedWidthAndHeight(int width, int height, int format,
     } else {
         switch (format)
         {
+            case HAL_PIXEL_FORMAT_YCrCb_420_SP:
             case HAL_PIXEL_FORMAT_YCrCb_420_SP_ADRENO:
             case HAL_PIXEL_FORMAT_RAW_SENSOR:
                 aligned_w = ALIGN(width, 32);
@@ -189,7 +190,6 @@ void AdrenoMemInfo::getAlignedWidthAndHeight(int width, int height, int format,
                 aligned_w = ALIGN(width, 128);
                 break;
             case HAL_PIXEL_FORMAT_YCbCr_420_SP:
-            case HAL_PIXEL_FORMAT_YCrCb_420_SP:
             case HAL_PIXEL_FORMAT_YV12:
             case HAL_PIXEL_FORMAT_YCbCr_422_SP:
             case HAL_PIXEL_FORMAT_YCrCb_422_SP:
