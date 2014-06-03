19c4ab9829ac810413424f384ab107abd9a1c27f
Praveena Pachipulusu <pveena@codeaurora.org>
2014-06-03 18:23:16 +0530

    libgralloc: Set Alignment to 1M only for Secured buffers.
    Set Data alignment to 1M only for secureed buffers which
    are allocated via MM heap. IOMMU heap allocated buffers
    should have buffer alignment of 4k.
    
    Change-Id: I501b35ab103b687f1081338e0c100c29e0b9c4bf

--- libgralloc/gpu.cpp
+++ libgralloc/gpu.cpp
@@ -66,8 +66,9 @@ int gpu_context_t::gralloc_alloc_buffer(size_t size, int usage,
 
     /* force 1MB alignment selectively for secure buffers, MDP5 onwards */
 #ifdef MDSS_TARGET
-    if (usage & GRALLOC_USAGE_PROTECTED) {
-        data.align = ALIGN(data.align, SZ_1M);
+    if ((usage & GRALLOC_USAGE_PROTECTED) &&
+        (usage & GRALLOC_USAGE_PRIVATE_MM_HEAP)) {
+        data.align = ALIGN((int) data.align, SZ_1M);
         size = ALIGN(size, data.align);
     }
 #endif
