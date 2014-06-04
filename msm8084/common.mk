18d878af97583ff40eebb216ae7ea830eb2197d8
Ajay Dudani <adudani@codeaurora.org>
2014-06-03 22:40:02 -0700

    apq8084: display: Drop dependency on KERNEL_OBJS by default
    Change-Id: I51fa6eec05ed4057a768a2e2e06739b3d3fc1bbd

--- msm8084/common.mk
+++ msm8084/common.mk
@@ -44,12 +44,3 @@ endif
 ifneq ($(call is-platform-sdk-version-at-least,18),true)
     common_flags += -DANDROID_JELLYBEAN_MR1=1
 endif
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-# This check is to pick the kernel headers from the right location.
-# If the macro above is defined, we make the assumption that we have the kernel
-# available in the build tree.
-# If the macro is not present, the headers are picked from hardware/qcom/msmXXXX
-# failing which, they are picked from bionic.
-    common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-    kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-endif
