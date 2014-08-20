342a007617239522494016543438b51906b59843
Shuzhen Wang <shuzhenw@codeaurora.org>
2014-08-20 00:15:51 -0700

    gralloc: Fix RAW16 stride
    RAW16 stride should be multiple of 16 pixels, not 32.
    
    Change-Id: Ic5f6920402ba9600b983e457cfc6863888345fe8

--- libgralloc/alloc_controller.cpp
+++ libgralloc/alloc_controller.cpp
@@ -161,8 +161,10 @@ void AdrenoMemInfo::getAlignedWidthAndHeight(int width, int height, int format,
     {
         case HAL_PIXEL_FORMAT_YCrCb_420_SP:
         case HAL_PIXEL_FORMAT_YCrCb_420_SP_ADRENO:
-        case HAL_PIXEL_FORMAT_RAW_SENSOR:
             aligned_w = ALIGN(width, 32);
+            break;
+        case HAL_PIXEL_FORMAT_RAW_SENSOR:
+            aligned_w = ALIGN(width, 16);
             break;
         case HAL_PIXEL_FORMAT_RAW10:
             aligned_w = ALIGN(width * 10 /8, 16);
