3c56d6608eb0d6c8a6678bde3a9ffb18fac237ed
Ethan Chen <intervigil@gmail.com>
2014-10-03 15:02:45 -0700

    display: Use project pathmap
    Change-Id: I2a51d48a4338f3aeb65be5d2c0b88eb12b6df771

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),)
+ifeq ($(call my-dir),$(call project-path-for,qcom-display))
 ifneq ($(filter msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
     #This is for 8974 based platforms
     include $(call all-named-subdir-makefiles,msm8974)
