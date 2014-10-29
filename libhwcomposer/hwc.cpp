fa21b1389890bc2fff874e5b6a97a1c1ad951e64
Naseer Ahmed <naseer@codeaurora.org>
2014-10-28 20:20:04 -0400

    hwc: Handle DOZE_SUSPEND properly
    DOZE_SUSPEND is the lowest power state with the display still on.
    With DOZE, we can still have updates. With DOZE_SUSPEND, MDP
    clocks are off and no update can happen. This is applicable only for
    wearables and doesn't happen in doze in handhelds/tablets.
    
    Change-Id: I10fe467a099ead3b0b56a01fec0fb18e1ad78580

--- libhwcomposer/hwc.cpp
+++ libhwcomposer/hwc.cpp
@@ -426,7 +426,13 @@ static int hwc_setPowerMode(struct hwc_composer_device_1* dev, int dpy,
             value = FB_BLANK_POWERDOWN;
             break;
         case HWC_POWER_MODE_DOZE:
+            // FB_BLANK_NORMAL is being used here to map to doze mode
+            // This definition is specific to our fbdev implementation
+            value = FB_BLANK_NORMAL;
+            break;
         case HWC_POWER_MODE_DOZE_SUSPEND:
+            // FB_BLANK_VSYNC_SUSPEND is being used here to map to doze_suspend
+            // This definition is specific to our fbdev implementation
             value = FB_BLANK_VSYNC_SUSPEND;
             break;
         case HWC_POWER_MODE_NORMAL:
@@ -449,7 +455,8 @@ static int hwc_setPowerMode(struct hwc_composer_device_1* dev, int dpy,
             ctx->mHPDEnabled = true;
         }
 
-        ctx->dpyAttr[dpy].isActive =  not(mode == HWC_POWER_MODE_OFF);
+        ctx->dpyAttr[dpy].isActive = not(mode == HWC_POWER_MODE_OFF ||
+                mode == HWC_POWER_MODE_DOZE_SUSPEND);
 
         if(ctx->mVirtualonExtActive) {
             /* if mVirtualonExtActive is true, display hal will
