f606d4bb9e0f6f5a0a9b765c2a938b908d474895
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-06 19:42:54 +0000

    Under CM, always assume kernel source is present
    Change-Id: Ic860035406ffb3d181b41249cd63bbf6cf5f32fb

--- msm8996/common.mk
+++ msm8996/common.mk
@@ -45,7 +45,7 @@ ifneq ($(call is-platform-sdk-version-at-least,18),true)
     common_flags += -DANDROID_JELLYBEAN_MR1=1
 endif
 
-ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
+#ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
 # This check is to pick the kernel headers from the right location.
 # If the macro above is defined, we make the assumption that we have the kernel
 # available in the build tree.
@@ -53,4 +53,4 @@ ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
 # failing which, they are picked from bionic.
     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-endif
+#endif
