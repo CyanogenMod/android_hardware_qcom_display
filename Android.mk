9a38fe2cf7a578fb9aa56818de1c9bcbbdb577f9
Ethan Chen <intervigil@gmail.com>
2014-11-08 23:25:53 -0800

    display: Use project pathmap
    Change-Id: Idebfe1e9477b5b05410df11071f681eec8d41c74

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(call my-dir)/$(TARGET_BOARD_PLATFORM),$(call project-path-for,qcom-display))
+
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(TARGET_BOARD_AUTO),true)
   ifneq ($(filter msm8084 msm8x84,$(TARGET_BOARD_PLATFORM)),)
@@ -29,4 +31,6 @@ ifneq ($(TARGET_BOARD_AUTO),true)
       endif
     endif
   endif
+endif
+
 endif
