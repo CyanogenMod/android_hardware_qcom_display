2550def04f94267836dcac313fa96c8e486230b0
Sushil Chauhan <sushilchauhan@codeaurora.org>
2014-12-22 14:54:23 -0800

    sde: Get new metadata values from driver
    Get new metadata values: max pipe bandwidth, max sde clock rate and
    clock fudge factor. They are needed for bandwidth and clock checks.
    
    Change-Id: I16078c6a707f5a81aa0eb2690b756df732fec218

--- displayengine/include/core/display_interface.h
+++ displayengine/include/core/display_interface.h
@@ -94,6 +94,7 @@ struct DisplayConfigVariableInfo {
   float y_dpi;                //!< Dots per inch in Y-direction.
   float fps;                  //!< Frame rate per second.
   uint32_t vsync_period_ns;   //!< VSync period in nanoseconds.
+  uint32_t v_total;           //!< Total lines in Y-direction (vActive + vFP + vBP + vPulseWidth).
 
   DisplayConfigVariableInfo() : x_pixels(0), y_pixels(0), x_dpi(0.0f), y_dpi(0.0f),
                                fps(0.0f), vsync_period_ns(0) { }
