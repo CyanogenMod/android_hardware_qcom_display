c99073234f541f13353763e0fa1d2c20de5b8e6a
Justin Philip <jphili@codeaurora.org>
2015-01-06 11:55:12 +0530

    libhwcomposer: Enable padding round for SMP failure
    SMP failure for video only composition should introduce
    padding round to free up pipes.
    
    Change-Id: I6fca32d53f1fc515ca4861d700566c7e02edeee5

--- libhwcomposer/hwc_mdpcomp.cpp
+++ libhwcomposer/hwc_mdpcomp.cpp
@@ -1086,6 +1086,11 @@ bool MDPComp::videoOnlyComp(hwc_context_t *ctx,
 
     if(!postHeuristicsHandling(ctx, list)) {
         ALOGD_IF(isDebug(), "post heuristic handling failed");
+        if(errno == ENOBUFS) {
+            ALOGD_IF(isDebug(), "SMP Allocation failed");
+            //On SMP allocation failure in video only comp add padding round
+            ctx->isPaddingRound = true;
+        }
         reset(ctx);
         return false;
     }
