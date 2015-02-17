f48658370b1f005cb58fe8367c1bba29fc85deca
Saurabh Shah <saurshah@codeaurora.org>
2015-02-17 09:57:47 -0800

    hwc: Integerize in the outward direction of rectangle
    Currently HWC integerizes float source co-ordinates (l, t, b, r) in
    the inward direction, mimicking SF from pre-HWC-1.4. In case of
    upscale where we need to create more destination pixels than incoming
    ones, this chopping off can lead to quality issues, especially given
    that the error magnification can be as much as 20x.
    
    This change integerizes the source rectangle in the outward direction
    which will help magnification cases. Downscale cases should be
    unaffected since reduction hides errors, unlike magnification.
    
    Change-Id: Ib72a1f8d52f4ec800387bfe2b91c9d3a65e6f557

--- libhwcomposer/hwc_utils.h
+++ libhwcomposer/hwc_utils.h
@@ -214,11 +214,11 @@ inline overlay::Rotator* LayerRotMap::getRot(uint32_t index) const {
 }
 
 inline hwc_rect_t integerizeSourceCrop(const hwc_frect_t& cropF) {
-    hwc_rect_t cropI = {0};
-    cropI.left = int(ceilf(cropF.left));
-    cropI.top = int(ceilf(cropF.top));
-    cropI.right = int(floorf(cropF.right));
-    cropI.bottom = int(floorf(cropF.bottom));
+    hwc_rect_t cropI = {0,0,0,0};
+    cropI.left = int(floorf(cropF.left));
+    cropI.top = int(floorf(cropF.top));
+    cropI.right = int(ceilf(cropF.right));
+    cropI.bottom = int(ceilf(cropF.bottom));
     return cropI;
 }
 
