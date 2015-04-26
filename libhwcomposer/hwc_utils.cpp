631b0b5e2f8f74b5b37f995ed2c792baf2ba4f00
Łukasz Domeradzki <JustArchi@JustArchi.net>
2015-04-27 00:50:44 +0200

    hwc: O3 fixes
    Change-Id: I38e6ae18c187697b6b95d82d18567d36e8c904a9

--- libhwcomposer/hwc_utils.cpp
+++ libhwcomposer/hwc_utils.cpp
@@ -1234,7 +1234,7 @@ void setMdpFlags(hwc_layer_1_t *layer,
     }
 }
 
-inline int configRotator(Rotator *rot, Whf& whf,
+int configRotator(Rotator *rot, Whf& whf,
         const Whf& origWhf, const eMdpFlags& mdpFlags,
         const eTransform& orient,
         const int& downscale) {
@@ -1305,7 +1305,7 @@ ovutils::eDest getPipeForFb(hwc_context_t *ctx, int dpy) {
     return ov.nextPipe(ovutils::OV_MDP_PIPE_VG, dpy);
 }
 
-inline int configMdp(Overlay *ov, const PipeArgs& parg,
+int configMdp(Overlay *ov, const PipeArgs& parg,
         const eTransform& orient, const hwc_rect_t& crop,
         const hwc_rect_t& pos, const MetaData_t *metadata,
         const eDest& dest) {
@@ -1331,7 +1331,7 @@ inline int configMdp(Overlay *ov, const PipeArgs& parg,
     return 0;
 }
 
-inline void updateSource(eTransform& orient, Whf& whf,
+void updateSource(eTransform& orient, Whf& whf,
         hwc_rect_t& crop) {
     Dim srcCrop(crop.left, crop.top,
             crop.right - crop.left,
