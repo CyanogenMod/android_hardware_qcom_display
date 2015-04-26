d4679a2f417c077eea6cd9677edb6db19fa811cd
Rashed Abdel-Tawab <rashed@linux.com>
2015-04-25 21:12:44 -0400

    msm8994: libhdmi: Add dependency on kernel headers
    Refuses to build otherwise
    
    Change-Id: I1001b51cb289ec30edfe799175e6d40b596af2fa

--- libhdmi/Android.mk
+++ libhdmi/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) liboverlay libqdutils
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhdmi\"
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES               := hdmi.cpp
 
 include $(BUILD_SHARED_LIBRARY)
