1e781f155718c77806745a3a13f828f0c187043b
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-05-03 23:57:56 -0400

    libhwcomposer: remove outdated mdpVersion
    Change-Id: I8e5b23f6098487514e9e3c4323e10d211af8347a

--- libhwcomposer/hwc_utils.cpp
+++ libhwcomposer/hwc_utils.cpp
@@ -1038,7 +1038,7 @@ int hwc_sync(hwc_context_t *ctx, hwc_display_contents_1_t* list, int dpy,
 
 #ifndef MDSS_TARGET
     //Send acquireFenceFds to rotator
-    if(mdpVersion >= qdutils::MDP_V4_0 && mdpVersion < qdutils::MDSS_V5) {
+    if(mdpVersion < qdutils::MDSS_V5) {
         //A-family
         int rotFd = ctx->mRotMgr->getRotDevFd();
         struct msm_rotator_buf_sync rotData;
@@ -1140,7 +1140,7 @@ int hwc_sync(hwc_context_t *ctx, hwc_display_contents_1_t* list, int dpy,
         ctx->mCopyBit[dpy]->setReleaseFd(releaseFd);
 
     //A-family
-    if(mdpVersion >= qdutils::MDP_V4_0 && mdpVersion < qdutils::MDSS_V5) {
+    if(mdpVersion < qdutils::MDSS_V5) {
         //Signals when MDP finishes reading rotator buffers.
         ctx->mLayerRotMap[dpy]->setReleaseFd(releaseFd);
     }
