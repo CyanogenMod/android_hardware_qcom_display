e6f11cef3d98b5d5848d92fcf9a60cbe8ea60ef2
Praveena Pachipulusu <pveena@codeaurora.org>
2015-05-29 19:38:30 +0530

    hwc: Reset the refresh rate alone to avoid stack corruption.
    In early return calls of prepare, reset the refresh rate
    irrespective of layer properties to avoid stack corruption.
    
    Change-Id: I5a04e111b1f10a6cb807459c7693d049c454edaa
    CRs-Fixed: 838902

--- libhwcomposer/hwc_mdpcomp.cpp
+++ libhwcomposer/hwc_mdpcomp.cpp
@@ -1993,7 +1993,8 @@ int MDPComp::prepare(hwc_context_t *ctx, hwc_display_contents_1_t* list) {
                 __FUNCTION__);
         mCachedFrame.reset();
 #ifdef DYNAMIC_FPS
-        setDynRefreshRate(ctx, list);
+        // Reset refresh rate
+        setRefreshRate(ctx, mDpy, ctx->dpyAttr[mDpy].refreshRate);
 #endif
         return -1;
     }
@@ -2009,7 +2010,8 @@ int MDPComp::prepare(hwc_context_t *ctx, hwc_display_contents_1_t* list) {
         setMDPCompLayerFlags(ctx, list);
         mCachedFrame.updateCounts(mCurrentFrame);
 #ifdef DYNAMIC_FPS
-        setDynRefreshRate(ctx, list);
+        // Reset refresh rate
+        setRefreshRate(ctx, mDpy, ctx->dpyAttr[mDpy].refreshRate);
 #endif
         ret = -1;
         return ret;
