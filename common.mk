b1d1e0ce2cb7a34a4504fa411dfec9c315fd8775
Scott Mertz <scott@cyngn.com>
2015-06-14 14:05:46 -0700

    Revert "hqd: Use MSM_VIDC_TARGET_LIST for Venus color format"
    This reverts commit 103f6669f4ef55fcd6b9495eb42b989f49f9410f.
    
    We don't use this list in CM
    
    Change-Id: I9a7dd9e9f833590bf9e53b8467e97d235ad0208c

--- common.mk
+++ common.mk
@@ -25,7 +25,8 @@ ifeq ($(ARCH_ARM_HAVE_NEON),true)
     common_flags += -D__ARM_HAVE_NEON
 endif
 
-ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)), true)
+ifeq ($(call is-board-platform-in-list, msm8974 msm8226 msm8610 apq8084 \
+        mpq8092 msm_bronze msm8916 msm8994 msm8992), true)
     common_flags += -DVENUS_COLOR_FORMAT
     common_flags += -DMDSS_TARGET
 endif
