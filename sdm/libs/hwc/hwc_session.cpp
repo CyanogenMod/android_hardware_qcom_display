4eee60d6bd831b0d22d54d9c565d05a8eff048dd
Zohaib Alam <zalam@codeaurora.org>
2015-07-01 11:44:25 -0400

    sdm: displayapi: Fix the method of brightness value consumption
    Use resp_payload to retrieve brightness value from colormanager,
    instead of using pending_action.
    
    Change-Id: I3102675c3ec37d7128cf6c58dcb7a0a7af213017

--- sdm/libs/hwc/hwc_session.cpp
+++ sdm/libs/hwc/hwc_session.cpp
@@ -700,7 +700,7 @@ void HWCSession::DynamicDebug(const android::Parcel *input_parcel) {
 
 android::status_t HWCSession::QdcmCMDHandler(const android::Parcel &in, android::Parcel *out) {
   int ret = 0;
-  int *brightness_value = NULL;
+  int32_t *brightness_value = NULL;
   uint32_t display_id(0);
   PPPendingParams pending_action;
   PPDisplayAPIPayload resp_payload, req_payload;
@@ -738,15 +738,13 @@ android::status_t HWCSession::QdcmCMDHandler(const android::Parcel &in, android:
     case kDisableSolidFill:
       break;
     case kSetPanelBrightness:
-      brightness_value = reinterpret_cast<int*>(pending_action.params);
-      pending_action.params = NULL;
+      brightness_value = reinterpret_cast<int32_t*>(resp_payload.payload);
       if (brightness_value == NULL) {
-        DLOGE("Brightness is null");
+        DLOGE("Brightness value is Null");
         return -EINVAL;
       }
       if (HWC_DISPLAY_PRIMARY == display_id)
         ret = hwc_display_[HWC_DISPLAY_PRIMARY]->SetPanelBrightness(*brightness_value);
-      delete brightness_value;
       break;
     case kNoAction:
       break;
