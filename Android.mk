8e173c247282e12b7a82a9d0c2218ffaf0e9ef58
Steve Paik <spaik@google.com>
2015-07-15 16:50:53 -0700

    Workaround for adp8064
    This is a squash of Ia669f72f3bac9e745d7241b2cf70bd4c33007922
    and I4facb1e301462833115955655029a9431690344a
    
    Change-Id: Icf59682cbb88df06055da09ca393af37101e7cd0

--- Android.mk
+++ Android.mk
@@ -1,21 +1,24 @@
-ifneq ($(filter msm8084 msm8x84,$(TARGET_BOARD_PLATFORM)),)
+# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
+ifneq ($(TARGET_BOARD_AUTO),true)
+  ifneq ($(filter msm8084 msm8x84,$(TARGET_BOARD_PLATFORM)),)
     #This is for 8084 based platforms
     include $(call all-named-subdir-makefiles,msm8084)
-else
-ifneq ($(filter msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
-    #This is for 8974 based (and B-family) platforms
-    include $(call all-named-subdir-makefiles,msm8974)
-else
-ifneq ($(filter msm8226 msm8x26,$(TARGET_BOARD_PLATFORM)),)
-    include $(call all-named-subdir-makefiles,msm8226)
-else
-ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
-    include $(call all-named-subdir-makefiles,msm8960)
-else
-ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
-    include $(call all-named-subdir-makefiles,msm8994)
-endif
-endif
-endif
-endif
+  else
+    ifneq ($(filter msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
+      #This is for 8974 based (and B-family) platforms
+      include $(call all-named-subdir-makefiles,msm8974)
+    else
+      ifneq ($(filter msm8226 msm8x26,$(TARGET_BOARD_PLATFORM)),)
+        include $(call all-named-subdir-makefiles,msm8226)
+      else
+        ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+          include $(call all-named-subdir-makefiles,msm8960)
+        else
+          ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
+            include $(call all-named-subdir-makefiles,msm8994)
+          endif
+        endif
+      endif
+    endif
+  endif
 endif
