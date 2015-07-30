e787316d7461bafd0625a031d33968388489ef34
M1cha <sigmaepsilon92@gmail.com>
2015-07-30 22:18:10 +0200

    liblight: add support for battery lights
    Change-Id: I52c348406551734988e7fbefa95660398b9bfb66

--- liblight/lights.c
+++ liblight/lights.c
@@ -237,6 +237,17 @@ handle_speaker_battery_locked(struct light_device_t* dev)
 }
 
 static int
+set_light_battery(struct light_device_t* dev,
+        struct light_state_t const* state)
+{
+    pthread_mutex_lock(&g_lock);
+    g_battery = *state;
+    handle_speaker_battery_locked(dev);
+    pthread_mutex_unlock(&g_lock);
+    return 0;
+}
+
+static int
 set_light_notifications(struct light_device_t* dev,
         struct light_state_t const* state)
 {
@@ -291,6 +302,8 @@ static int open_lights(const struct hw_module_t* module, char const* name,
         set_light = set_light_backlight;
     else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
         set_light = set_light_notifications;
+    else if (0 == strcmp(LIGHT_ID_BATTERY, name))
+        set_light = set_light_battery;
     else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
         set_light = set_light_attention;
     else
