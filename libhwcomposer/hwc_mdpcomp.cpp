3a48a690bcec3fb5433922b7391584ffdf8bf9c7
Dileep Kumar Reddi <dkumarre@codeaurora.org>
2015-08-06 11:28:27 +0530

    hwc: Do not create idle invalidator for MDP3 targets
    We are not using idle invalidator for MDP3 targets,
    so avoiding creating idele invalidator instance here.
    
    Change-Id: I8d1fc1b19dc52c47a3d8b281cb7e8b46e26f116d

--- libhwcomposer/hwc_mdpcomp.cpp
+++ libhwcomposer/hwc_mdpcomp.cpp
@@ -137,7 +137,8 @@ bool MDPComp::init(hwc_context_t *ctx) {
             sMaxPipesPerMixer = min(val, MAX_PIPES_PER_MIXER);
     }
 
-    if(ctx->mMDP.panel != MIPI_CMD_PANEL) {
+    if(ctx->mMDP.panel != MIPI_CMD_PANEL &&
+            (ctx->mMDP.version >= qdutils::MDP_V4_0)) {
         sIdleInvalidator = IdleInvalidator::getInstance();
         if(sIdleInvalidator->init(timeout_handler, ctx) < 0) {
             delete sIdleInvalidator;
