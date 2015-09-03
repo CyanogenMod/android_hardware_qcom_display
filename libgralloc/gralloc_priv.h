192b634b01e3346b2db653c221009c71cee66d62
Arun Kumar K.R <akumarkr@codeaurora.org>
2015-09-03 11:37:35 -0700

    gralloc: Add a private flag for INTERNAL only
    Add PRIV_FLAGS_INTERNAL_ONLY which will be used by clients
    to know if that is an INTERNAL only layer.
    
    Change-Id: I2cc69bcbc86c18d97ce0c841670b760e48721ffa

--- libgralloc/gralloc_priv.h
+++ libgralloc/gralloc_priv.h
@@ -158,6 +158,7 @@ struct private_handle_t : public native_handle {
             PRIV_FLAGS_EXTERNAL_ONLY      = 0x00002000,
             // Set by HWC for protected non secure buffers
             PRIV_FLAGS_PROTECTED_BUFFER   = 0x00004000,
+            PRIV_FLAGS_INTERNAL_ONLY      = 0x00008000,
             PRIV_FLAGS_VIDEO_ENCODER      = 0x00010000,
             PRIV_FLAGS_CAMERA_WRITE       = 0x00020000,
             PRIV_FLAGS_CAMERA_READ        = 0x00040000,
