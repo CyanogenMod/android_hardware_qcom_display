fe83f20df8b3607e74fdabd37c5bfdeddc8aefe5
Saurabh Shah <saurshah@codeaurora.org>
2015-09-04 14:27:58 -0700

    hwc: Complete DMA transition with a refresh
    In case of secure videos, if DMA pipe transition is needed and if
    there are no subsequent draw rounds, black content continues to be
    shown. This change triggers a refresh in such cases to complete the
    DMA pipe transition. It also allows composition to go through if the
    previous and current frame are same with GPU composition, if secure
    videos are present.
    
    Change-Id: I5202f0f170ff5d1a0f8280fcf385a23749b5d0a4

--- libhwcomposer/hwc_utils.cpp
+++ libhwcomposer/hwc_utils.cpp
@@ -410,6 +410,7 @@ int initContext(hwc_context_t *ctx)
 
     memset(&(ctx->mPtorInfo), 0, sizeof(ctx->mPtorInfo));
     ctx->mHPDEnabled = false;
+    ctx->triggerRefresh = false;
     ALOGI("Initializing Qualcomm Hardware Composer");
     ALOGI("MDP version: %d", ctx->mMDP.version);
 
