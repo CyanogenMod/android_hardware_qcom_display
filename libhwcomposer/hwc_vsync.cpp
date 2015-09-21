2ca9c7a72df64d6d8291d1b5fa6cea65f0364c6f
Steve Kondik <steve@cyngn.com>
2015-09-20 17:11:20 -0700

    hwc: Set ioprio for vsync thread
     * We're not doing this automatically anymore.
    
    Change-Id: I0ba52fe4648938bd7a0309bc03dc439e3d906970

--- libhwcomposer/hwc_vsync.cpp
+++ libhwcomposer/hwc_vsync.cpp
@@ -19,6 +19,7 @@
  */
 
 #include <cutils/properties.h>
+#include <cutils/iosched_policy.h>
 #include <utils/Log.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
@@ -95,6 +96,7 @@ static void *vsync_loop(void *param)
     prctl(PR_SET_NAME, (unsigned long) &thread_name, 0, 0, 0);
     androidSetThreadPriority(0, HAL_PRIORITY_URGENT_DISPLAY +
                 android::PRIORITY_MORE_FAVORABLE);
+    android_set_rt_ioprio(0, 1);
 
     char vdata[MAX_DATA];
     //Number of physical displays
