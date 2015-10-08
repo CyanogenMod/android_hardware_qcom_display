ed75fdb88b24c66eb2ff9dd2201fe6f5bb66ef9f
Dileep Kumar Reddi <dkumarre@codeaurora.org>
2015-10-08 19:48:03 +0530

    hwc: Reset refresh-rate for few cases as part of timeout handler
    We do not obey idlefallback in certain usecases like secure display,
    secure RGB layers, only a single layer being composed or yuv-layers.
    Do not set refresh rate to minimum for those usecases as part of
    composition thread but set it as part of timeout handler.
    
    Change-Id: I743e44c2bc2c082767ba6b153e714e9ee73794e8

--- libhwcomposer/hwc_mdpcomp.cpp
+++ libhwcomposer/hwc_mdpcomp.cpp
@@ -2190,7 +2190,10 @@ void MDPComp::setDynRefreshRate(hwc_context_t *ctx, hwc_display_contents_1_t* li
                                         ctx->mUseMetaDataRefreshRate) {
         uint32_t refreshRate = ctx->dpyAttr[mDpy].refreshRate;
         MDPVersion& mdpHw = MDPVersion::getInstance();
-        if(sIdleFallBack) {
+        if(sIdleFallBack && !ctx->listStats[mDpy].secureUI &&
+                !ctx->listStats[mDpy].secureRGBCount &&
+                (ctx->listStats[mDpy].numAppLayers > 1) &&
+                (ctx->listStats[mDpy].yuvCount == 0)) {
             //Set minimum panel refresh rate during idle timeout
             refreshRate = mdpHw.getMinFpsSupported();
         } else if(onlyVideosUpdating(ctx, list)) {
@@ -2546,7 +2549,8 @@ bool MDPCompNonSplit::draw(hwc_context_t *ctx, hwc_display_contents_1_t* list) {
     if(sIdleInvalidator && !sIdleFallBack &&
        /* Neednot set for single pipe mdp composition cases */
        !(mCurrentFrame.mdpCount == 1 and mCurrentFrame.fbCount == 0) &&
-       !(ctx->listStats[mDpy].yuvCount == 0)) {
+       !ctx->listStats[mDpy].secureUI && !ctx->listStats[mDpy].secureRGBCount &&
+       (ctx->listStats[mDpy].yuvCount == 0)) {
         sHandleTimeout = true;
     }
 
@@ -2819,7 +2823,8 @@ bool MDPCompSplit::draw(hwc_context_t *ctx, hwc_display_contents_1_t* list) {
          needs3DComposition(ctx, HWC_DISPLAY_EXTERNAL)) &&
        /* Neednot set for single pipe mdp composition cases */
        !(mCurrentFrame.mdpCount == 1 and mCurrentFrame.fbCount == 0) &&
-       !(ctx->listStats[mDpy].yuvCount == 0) ) {
+       !ctx->listStats[mDpy].secureUI && !ctx->listStats[mDpy].secureRGBCount &&
+       (ctx->listStats[mDpy].yuvCount == 0) ) {
         sHandleTimeout = true;
     }
 
