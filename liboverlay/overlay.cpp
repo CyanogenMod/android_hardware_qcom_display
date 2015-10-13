peat-psuwit/ubuntu-phone_hardware_qcom_display-caf-new

overlay: Add a flag for compilation with new API
 * ROI fields have changed for dual-dsi systems. Add
   an ifdef to allow compilation with old kernels.

Change-Id: Ie18404bba2c5afe4e8b913c1b4eee2a588f37acb

liboverlay/overlay.cpp
@@ -469,6 +469,7 @@ bool Overlay::displayCommit(const int& fd, const utils::Dim& lRoi,
     struct mdp_display_commit info;
     memset(&info, 0, sizeof(struct mdp_display_commit));
     info.flags = MDP_DISPLAY_COMMIT_OVERLAY;
+#ifdef DUAL_DSI
     info.l_roi.x = lRoi.x;
     info.l_roi.y = lRoi.y;
     info.l_roi.w = lRoi.w;
@@ -477,7 +478,12 @@ bool Overlay::displayCommit(const int& fd, const utils::Dim& lRoi,
     info.r_roi.y = rRoi.y;
     info.r_roi.w = rRoi.w;
     info.r_roi.h = rRoi.h;
-
+#else
+    info.roi.x = lRoi.x;
+    info.roi.y = lRoi.y;
+    info.roi.w = lRoi.w;
+    info.roi.h = lRoi.h;
+#endif
     if(!mdp_wrapper::displayCommit(fd, info)) {
         ALOGE("%s: commit failed", __func__);
         return false;

CyanogenMod/android_hardware_qcom_display

liboverlay/overlay.cpp
@@ -356,10 +356,18 @@ bool Overlay::displayCommit(const int& fd, const utils::Dim& roi) {
     struct mdp_display_commit info;
     memset(&info, 0, sizeof(struct mdp_display_commit));
     info.flags = MDP_DISPLAY_COMMIT_OVERLAY;
+
+#ifdef DUAL_DSI // new kernel api
+    info.l_roi.x = roi.x;
+    info.l_roi.y = roi.y;
+    info.l_roi.w = roi.w;
+    info.l_roi.h = roi.h;
+#else
     info.roi.x = roi.x;
     info.roi.y = roi.y;
     info.roi.w = roi.w;
     info.roi.h = roi.h;
+#endif
 
     if(!mdp_wrapper::displayCommit(fd, info)) {
         ALOGE("%s: commit failed", __func__);