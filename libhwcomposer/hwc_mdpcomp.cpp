580ab63b1eee90de40f51e56557665bf96ad7ce1
Arun Kumar K.R <akumarkr@codeaurora.org>
2015-10-19 11:53:13 +0530

    hwc: Start with ROI set to destination
    Some layers will not have surfaceDamage information,
    like video and CPU rendered, so consider their destination
    as the surfaceDamage. For other layers start with destination
    will be calculated based on the surfaceDamage
    
    Change-Id: Ic7308bf9dfb7d062596873963c738f644b1c1a84

--- libhwcomposer/hwc_mdpcomp.cpp
+++ libhwcomposer/hwc_mdpcomp.cpp
@@ -578,7 +578,8 @@ void MDPCompNonSplit::generateROI(hwc_context_t *ctx,
         hwc_layer_1_t* layer = &list->hwLayers[index];
         if (layerUpdating(layer) ||
                 isYuvBuffer((private_handle_t *)layer->handle)) {
-            hwc_rect_t dirtyRect = (struct hwc_rect){0, 0, 0, 0};;
+            hwc_rect_t dirtyRect = getIntersection(layer->displayFrame,
+                                                        fullFrame);
             if(!needsScaling(layer) && !layer->transform &&
                    (!isYuvBuffer((private_handle_t *)layer->handle)))
             {
@@ -687,8 +688,10 @@ void MDPCompSplit::generateROI(hwc_context_t *ctx,
         hwc_layer_1_t* layer = &list->hwLayers[index];
         private_handle_t *hnd = (private_handle_t *)layer->handle;
         if (layerUpdating(layer) || isYuvBuffer(hnd)) {
-            hwc_rect_t l_dirtyRect = (struct hwc_rect){0, 0, 0, 0};
-            hwc_rect_t r_dirtyRect = (struct hwc_rect){0, 0, 0, 0};
+            hwc_rect_t l_dirtyRect = getIntersection(layer->displayFrame,
+                                                     l_frame);
+            hwc_rect_t r_dirtyRect = getIntersection(layer->displayFrame,
+                                                     r_frame);
             if(!needsScaling(layer) && !layer->transform)
             {
                 l_dirtyRect = calculateDirtyRect(layer, l_frame);
