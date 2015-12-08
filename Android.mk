07bbf1e89c031a5d41a7561433e832d396c311a5
Prashant Malani <pmalani@google.com>
2015-12-07 17:46:35 -0800

    msm8909: Populate display code for msm8x09
    Copied from manifest tag LW.BR.1.0-00110-8x09w.0 for
    LW.BR.1.0
    
    Bug: 25691995
    Change-Id: I42c50111d92ecbdc9198ce4f2a98c26300dfa715

--- Android.mk
+++ Android.mk
@@ -14,6 +14,10 @@ ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
 else
 ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,msm8994)
+else
+ifneq ($(filter msm8909,$(TARGET_BOARD_PLATFORM)),)
+    include $(call all-named-subdir-makefiles,msm8909)
+endif
 endif
 endif
 endif
