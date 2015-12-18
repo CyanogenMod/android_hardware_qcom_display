8abd53155da9e500f50767c4b90ce3e246461a5e
codeworkx <codeworkx@cyanogenmod.org>
2015-12-18 22:27:12 +0100

    gralloc: add cflag to use nv21 format for camera preview
    Needed on LG G4 because with venus format we've wrong colors on preview.
    
    Broken since https://github.com/CyanogenMod/android_hardware_qcom_display/commit/de8d802d50e7d91e438eb868140ab649413f495d
    got merged.
    
    Change-Id: I1333c28317c31c08060ea5bef36669b3f1788b36

--- libgralloc/gpu.cpp
+++ libgralloc/gpu.cpp
@@ -300,9 +300,15 @@ int gpu_context_t::alloc_impl(int w, int h, int format, int usage,
             grallocFormat = HAL_PIXEL_FORMAT_YCrCb_420_SP; //NV21
         else if(usage & GRALLOC_USAGE_HW_CAMERA_WRITE) {
            if (format == HAL_PIXEL_FORMAT_YCbCr_420_888) {
+               ALOGE("%s: HAL_PIXEL_FORMAT_YCbCr_420_888 (%d)", __FUNCTION__, format);
                grallocFormat = HAL_PIXEL_FORMAT_NV21_ZSL; //NV21
            } else {
+               ALOGE("%s: else (%d)", __FUNCTION__, format);
+#ifdef CAMERA_PREVIEW_USES_NV21_FORMAT
+               grallocFormat = HAL_PIXEL_FORMAT_YCrCb_420_SP; //NV21 preview
+#else
                grallocFormat = HAL_PIXEL_FORMAT_YCbCr_420_SP_VENUS; //NV12 preview
+#endif
            }
         } else if(usage & GRALLOC_USAGE_HW_COMPOSER)
             //XXX: If we still haven't set a format, default to RGBA8888
