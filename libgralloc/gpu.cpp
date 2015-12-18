CyanogenMod/android_hardware_qcom_display

commit de8d802d50e7d91e438eb868140ab649413f495d

Change the IMPLEMENTATION_DEFINED preview buffers format
Set the preview buffers format be the same as the video buffers
in order to improve camera power usage

Change-Id: Iaf69dad9d15b0b6fdc4cfe538104f404008e84fb

libgralloc/gpu.cpp
@@ -298,9 +298,13 @@ int gpu_context_t::alloc_impl(int w, int h, int format, int usage,
             grallocFormat = HAL_PIXEL_FORMAT_NV21_ZSL; //NV21 ZSL
         else if(usage & GRALLOC_USAGE_HW_CAMERA_READ)
             grallocFormat = HAL_PIXEL_FORMAT_YCrCb_420_SP; //NV21
-        else if(usage & GRALLOC_USAGE_HW_CAMERA_WRITE)
-            grallocFormat = HAL_PIXEL_FORMAT_YCrCb_420_SP; //NV21
-        else if(usage & GRALLOC_USAGE_HW_COMPOSER)
+        else if(usage & GRALLOC_USAGE_HW_CAMERA_WRITE) {
+           if (format == HAL_PIXEL_FORMAT_YCbCr_420_888) {
+               grallocFormat = HAL_PIXEL_FORMAT_YCrCb_420_SP; //NV21
+           } else {
+               grallocFormat = HAL_PIXEL_FORMAT_YCbCr_420_SP_VENUS; //NV12 preview
+           }
+        } else if(usage & GRALLOC_USAGE_HW_COMPOSER)
             //XXX: If we still haven't set a format, default to RGBA8888
             grallocFormat = HAL_PIXEL_FORMAT_RGBA_8888;
     }

commit 0dfee1c74ee49636d910ffa4d47d181031a62aa4

gralloc: add cflag to use nv21 format for camera preview
Needed on LG G4 because with venus format we've wrong colors on preview.

Broken since https://github.com/CyanogenMod/android_hardware_qcom_display/commit/de8d802d50e7d91e438eb868140ab649413f495d
got merged.

Change-Id: I1333c28317c31c08060ea5bef36669b3f1788b36

libgralloc/gpu.cpp
@@ -302,7 +302,7 @@ int gpu_context_t::alloc_impl(int w, int h, int format, int usage,
            if (format == HAL_PIXEL_FORMAT_YCbCr_420_888) {
                grallocFormat = HAL_PIXEL_FORMAT_NV21_ZSL; //NV21
            } else {
-               grallocFormat = HAL_PIXEL_FORMAT_YCbCr_420_SP_VENUS; //NV12 preview
+               grallocFormat = HAL_PIXEL_FORMAT_YCrCb_420_SP; //NV12 preview
            }
         } else if(usage & GRALLOC_USAGE_HW_COMPOSER)
             //XXX: If we still haven't set a format, default to RGBA8888