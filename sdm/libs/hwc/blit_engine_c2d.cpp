c1cabe420e2e64805a8855b2ab4c1cf51c0eba1a
radhakrishna <radhakrishna@codeaurora.org>
2015-12-28 12:17:37 +0530

    sdm: Enable Hybrid composition
    Enable Hybrid composition based on property.
    
    Change-Id: I068300bceeac50eaa0e8e7596a677287550189fc

--- sdm/libs/hwc/blit_engine_c2d.cpp
+++ sdm/libs/hwc/blit_engine_c2d.cpp
@@ -57,6 +57,7 @@
 #include <utils/rect.h>
 
 #include "blit_engine_c2d.h"
+#include "hwc_debugger.h"
 
 #define __CLASS__ "BlitEngineC2D"
 
@@ -88,11 +89,13 @@ int BlitEngineC2d::RegionIterator::iterate(copybit_region_t const *self, copybit
   return 0;
 }
 
-BlitEngineC2d::BlitEngineC2d() : blit_active_(false), dump_frame_count_(0), dump_frame_index_(0) {
+BlitEngineC2d::BlitEngineC2d() {
   for (uint32_t i = 0; i < kNumBlitTargetBuffers; i++) {
     blit_target_buffer_[i] = NULL;
     release_fence_fd_[i] = -1;
   }
+
+  HWCDebugHandler::Get()->GetProperty("persist.hwc.blit.comp", &blit_supported_);
 }
 
 BlitEngineC2d::~BlitEngineC2d() {
@@ -250,8 +253,7 @@ int BlitEngineC2d::Prepare(LayerStack *layer_stack) {
 
   for (i = 0; i < layer_stack->layer_count; i++) {
     Layer &layer = layer_stack->layers[i];
-    if (IsUBWCFormat(layer.input_buffer->format)) {
-      // UBWC is not currently supported
+    if (!blit_supported_) {
       return -1;
     }
     if (layer.composition == kCompositionGPUTarget) {
