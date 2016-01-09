ca1644632f29ef367d578da1228b263161aed821
Arne Coucheron <arco68@gmail.com>
2016-01-09 13:16:37 +0100

    hwc: Fix compile errors after display-frameworks removal
    Change-Id: Ib8281f93d5e3faf5758417906a42bfe5ca794a8a

--- libhwcomposer/hwc_utils.h
+++ libhwcomposer/hwc_utils.h
@@ -26,6 +26,7 @@
 
 #include <fcntl.h>
 #include <math.h>
+#include <hardware/display_defs.h>
 #include <hardware/hwcomposer.h>
 #include <gr.h>
 #include <gralloc_priv.h>
@@ -49,10 +50,6 @@
 #define STR(f) #f;
 // Max number of PTOR layers handled
 #define MAX_PTOR_LAYERS 2
-
-#ifdef QTI_BSP
-#include <exhwcomposer_defs.h>
-#endif
 
 //Fwrd decls
 struct hwc_context_t;
