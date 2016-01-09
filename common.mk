a13ca36cae0ef29a74f1b24d43d6dbf32564b1bb
Arne Coucheron <arco68@gmail.com>
2016-01-09 13:16:37 +0100

    hwc: Fix compile errors after display-frameworks removal
    Change-Id: Ib8281f93d5e3faf5758417906a42bfe5ca794a8a

--- common.mk
+++ common.mk
@@ -45,7 +45,6 @@ kernel_includes :=
 ifeq ($(TARGET_USES_QCOM_BSP),true)
 # Enable QCOM Display features
     common_flags += -DQTI_BSP
-    common_includes += vendor/qcom/opensource/display-frameworks/include
 endif
 ifneq ($(call is-platform-sdk-version-at-least,18),true)
     common_flags += -DANDROID_JELLYBEAN_MR1=1
