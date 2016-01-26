009ad60ef34cad206b896b4abf1e8f9ed7e7fb15
Xu Yang <yangxu@codeaurora.org>
2016-01-26 17:23:00 +0800

    sdm: Set max_layers to 0 when HDMI is in S3D mode
    1. Set max_layers to 0 if HDMI is in S3D mode. So that primary
    display will fall back to GPU composition and release pipes for
    HMDI, especially in Top-Bottom S3D mode and 2k/4k S3D video cases.
    
    2. Change property name "sdm.hdmi.s3d_enable" to "sdm.hdmi.s3d_mode"
    to support top-bottom mode. The property value is: 1 for left-right
    mode, 2 for right-left mode, 3 for top-bottom mode, and 4 for frame
    pack mode.
    
    3. Populate S3D mode in panel info to strategy extension.
    
    CRs-Fixed: 977346
    Change-Id: If0fd5710206f862265f487ebb2705958c01fa9cc

--- sdm/include/private/extension_interface.h
+++ sdm/include/private/extension_interface.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2015, The Linux Foundation. All rights reserved.
+* Copyright (c) 2015 - 2016, The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted
 * provided that the following conditions are met:
@@ -58,7 +58,7 @@ class ExtensionInterface {
   virtual DisplayError DestroyPartialUpdate(PartialUpdateInterface *interface) = 0;
 
   virtual DisplayError CreateStrategyExtn(DisplayType type, HWDisplayMode mode,
-                                          StrategyInterface **interface) = 0;
+                                          HWS3DMode s3d_mode, StrategyInterface **interface) = 0;
   virtual DisplayError DestroyStrategyExtn(StrategyInterface *interface) = 0;
 
   virtual DisplayError CreateResourceExtn(const HWResourceInfo &hw_resource_info,
