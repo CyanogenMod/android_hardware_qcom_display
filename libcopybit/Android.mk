5458098911c930af074206668fd53d2b286f1be0
Dan Willemsen <dwillemsen@google.com>
2016-02-23 14:19:52 -0800

    Fix duplicate copy file rules
    If these libraries are built, both BUILD_COPY_FILES and
    BUILD_SHARED_LIBRARY would copy the headers. This would create a warning
    because multiple rules would define the same target header file.
    
    Bug: 27302058
    Change-Id: If91bd70d2320aaaee9679642ff9ea48b99739cbd
    (cherry picked from commit b360857e924f57588e29e47b047519ce097beed6)

--- libcopybit/Android.mk
+++ libcopybit/Android.mk
@@ -14,13 +14,13 @@
 
 LOCAL_PATH:= $(call my-dir)
 include $(LOCAL_PATH)/../common.mk
-include $(CLEAR_VARS)
 
+include $(CLEAR_VARS)
 LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
 LOCAL_COPY_HEADERS            := copybit.h copybit_priv.h
-#Copy the headers regardless of whether copybit is built
 include $(BUILD_COPY_HEADERS)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE                  := copybit.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS             := optional
