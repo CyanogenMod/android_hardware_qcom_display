fde98fd7cf57cf5951d4d0b2b5386c92c2c22972
Naseer Ahmed <naseer@codeaurora.org>
2016-03-14 19:58:14 -0400

    qservice: Allow camera server to call into HWC
    Change-Id: I535b97dcc7b7de0d87ef0eb4a2b6f9a82281c80f
    CRs-Fixed: 987009

--- libqservice/IQService.cpp
+++ libqservice/IQService.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2010 The Android Open Source Project
- * Copyright (C) 2012-2013, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2012-2016, The Linux Foundation. All rights reserved.
  *
  * Not a Contribution, Apache license notifications and license are
  * retained for attribution purposes only.
@@ -88,6 +88,7 @@ status_t BnQService::onTransact(
     const bool permission = (callerUid == AID_MEDIA ||
             callerUid == AID_GRAPHICS ||
             callerUid == AID_ROOT ||
+            callerUid == AID_CAMERASERVER ||
             callerUid == AID_SYSTEM);
 
     if (code == CONNECT) {
