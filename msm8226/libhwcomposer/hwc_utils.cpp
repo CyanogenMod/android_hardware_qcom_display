CyanogenMod/android_hardware_qcom_display

commit 3e344bb1ba683fec37dca28d0590e54678733a0e

display: HAL: FB pipe type select
When disable mdpcomp,it will fail to select pipe type.

For wearable targets, secondary display is not applicable.
Try for DMA pipe for FB first, if FB does not have scaling.

Change-Id: Ib621dca2f383d2dd9a6916bc8ccf6408050f8a5b
Signed-off-by: l00212897 <gang.li@huawei.com>
Signed-off-by: Uday Kishore Pasupuleti <upasupul@codeaurora.org>

msm8226/liboverlay/overlay.cpp
@@ -214,16 +214,26 @@ utils::eDest Overlay::getPipe_8x26(const PipeSpecs& pipeSpecs) {
     } else { //FB layer
         //For 8x26 Secondary we use DMA always for FB for inline rotation
         if(pipeSpecs.dpy == DPY_PRIMARY) {
-            dest = nextPipe(OV_MDP_PIPE_RGB, pipeSpecs.dpy, pipeSpecs.mixer);
-            if(dest == OV_INVALID) {
-                dest = nextPipe(OV_MDP_PIPE_VG, pipeSpecs.dpy, pipeSpecs.mixer);
+            /* For wearable targets, secondary display is not applicable
+               Try for DMA pipe for FB first, if FB does not have scaling*/
+            if (not pipeSpecs.needsScaling) {
+                dest = nextPipe(OV_MDP_PIPE_DMA, pipeSpecs.dpy, pipeSpecs.mixer);
+            } else {
+                dest = nextPipe(OV_MDP_PIPE_RGB, pipeSpecs.dpy, pipeSpecs.mixer);
+                if (dest == OV_INVALID) {
+                    // FB needs scaling, and RGB pipe is not available - try VG
+                    dest = nextPipe(OV_MDP_PIPE_VG, pipeSpecs.dpy, pipeSpecs.mixer);
+                }
             }
         }
         if(dest == OV_INVALID and (not pipeSpecs.needsScaling) and
-          (not (pipeSpecs.numActiveDisplays > 1 &&
-                pipeSpecs.dpy == DPY_PRIMARY))) {
+                (not (pipeSpecs.numActiveDisplays > 1 &&
+                      pipeSpecs.dpy == DPY_PRIMARY))) {
             dest = nextPipe(OV_MDP_PIPE_DMA, pipeSpecs.dpy, pipeSpecs.mixer);
         }
+    }
+    if (dest == OV_INVALID) {
+        ALOGE("%s - not able to allocate FB pipe", __func__);
     }
     return dest;
 }

commit 9fae8b8b5b928ba58a839d32e062069b7b561e2e

Fix rotation on hardware composer (msm8226)
Add RGBX_8888 as a supported format.

Also, fixes a mistake introduced by 3e344bb1ba683fec37dca28d0590e54678733a0e
If we try OV_MDP_PIPE_DMA and it doesn't work, try the other pipes
instead of just aborting.

Change-Id: Ice557d58abe700ff8b10c90c43bfbbfa371ab286

msm8226/libhwcomposer/hwc_utils.cpp
@@ -966,6 +966,7 @@ bool isRotatorSupportedFormat(private_handle_t *hnd) {
     // TODO: Add more formats in future, if mdp driver adds support
     switch(hnd->format) {
         case HAL_PIXEL_FORMAT_RGBA_8888:
+        case HAL_PIXEL_FORMAT_RGBX_8888:
         case HAL_PIXEL_FORMAT_RGB_565:
         case HAL_PIXEL_FORMAT_RGB_888:
         case HAL_PIXEL_FORMAT_BGRA_8888:

msm8226/liboverlay/overlay.cpp
@@ -218,12 +218,13 @@ utils::eDest Overlay::getPipe_8x26(const PipeSpecs& pipeSpecs) {
                Try for DMA pipe for FB first, if FB does not have scaling*/
             if (not pipeSpecs.needsScaling) {
                 dest = nextPipe(OV_MDP_PIPE_DMA, pipeSpecs.dpy, pipeSpecs.mixer);
-            } else {
+            }
+            if (dest == OV_INVALID) {
                 dest = nextPipe(OV_MDP_PIPE_RGB, pipeSpecs.dpy, pipeSpecs.mixer);
-                if (dest == OV_INVALID) {
-                    // FB needs scaling, and RGB pipe is not available - try VG
-                    dest = nextPipe(OV_MDP_PIPE_VG, pipeSpecs.dpy, pipeSpecs.mixer);
-                }
+            }
+            if (dest == OV_INVALID) {
+                // FB needs scaling, and RGB pipe is not available - try VG
+                dest = nextPipe(OV_MDP_PIPE_VG, pipeSpecs.dpy, pipeSpecs.mixer);
             }
         }
         if(dest == OV_INVALID and (not pipeSpecs.needsScaling) and