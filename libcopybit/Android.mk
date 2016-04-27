30b08db1498c0bb5e42e32bc147330f8afbd40b0
Khalid Zubair <kzubair@cyngn.com>
2016-04-27 16:06:18 -0700

    Fix warnings due to duplicate copy header rules
    The BUILD_SHARED_LIBRARY rules add a duplicate set of BUILD_COPY_HEADERS rules.
    Clearing the local vars fixes the following warnings:
    
    	build/core/copy_headers.mk:15: warning: overriding commands for target `/home/kzubair/cm/out/yam/12.1-yam/target/product/yam/obj/include/qcom/display/copybit.h'
    	build/core/copy_headers.mk:15: warning: ignoring old commands for target `/home/kzubair/cm/out/yam/12.1-yam/target/product/yam/obj/include/qcom/display/copybit.h'
    	build/core/copy_headers.mk:15: warning: overriding commands for target `/home/kzubair/cm/out/yam/12.1-yam/target/product/yam/obj/include/qcom/display/copybit_priv.h'
    	build/core/copy_headers.mk:15: warning: ignoring old commands for target `/home/kzubair/cm/out/yam/12.1-yam/target/product/yam/obj/include/qcom/display/copybit_priv.h'
    	build/core/copy_headers.mk:15: warning: overriding commands for target `/home/kzubair/cm/out/yam/12.1-yam/target/product/yam/obj/include/qcom/display/c2d2.h'
    	build/core/copy_headers.mk:15: warning: ignoring old commands for target `/home/kzubair/cm/out/yam/12.1-yam/target/product/yam/obj/include/qcom/display/c2d2.h'
    
    Change-Id: Ic2547734d32e0d6f6cd83116df272bbd2bca616c

--- libcopybit/Android.mk
+++ libcopybit/Android.mk
@@ -20,6 +20,7 @@ LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
 LOCAL_COPY_HEADERS            := copybit.h copybit_priv.h c2d2.h
 #Copy the headers regardless of whether copybit is built
 include $(BUILD_COPY_HEADERS)
+include $(CLEAR_VARS)
 
 LOCAL_MODULE                  := copybit.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH    := hw
