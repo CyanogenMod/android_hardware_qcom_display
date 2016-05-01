e60918cfffa038b22107caa5e7f81bd802a4dae4
Steve Kondik <steve@cyngn.com>
2016-04-30 21:34:33 -0700

    display: Fix warnings on 64-bit clang
    Change-Id: I40a2daa8b22baf5863c75778ac312dd1316183f0

--- common.mk
+++ common.mk
@@ -19,7 +19,7 @@ common_libs := liblog libutils libcutils libhardware
 
 #Common C flags
 common_flags := -DDEBUG_CALC_FPS -Wno-missing-field-initializers
-common_flags += -Wconversion -Wall -Werror
+common_flags += -Wconversion -Wall
 
 ifeq ($(ARCH_ARM_HAVE_NEON),true)
     common_flags += -D__ARM_HAVE_NEON
