68b10a4b1956757b656b0786ec5b8f5392b4f38e
feifanz <feifanz@codeaurora.org>
2016-06-06 16:59:59 +0800

    sdm: Add mode chose for dynamic framerate switch
    Add support for HDMI dynamic frame rate switch mode chose, it should
    be according to comparison with destination timing info.
    
    Change-Id: I46e58d745a9601b71d02b6d91b226616c6cb7a52
    CRs-fixed: 1032036

--- sdm/libs/core/fb/hw_hdmi.h
+++ sdm/libs/core/fb/hw_hdmi.h
@@ -49,7 +49,27 @@ class HWHDMI : public HWDevice {
     kModeVFP,
     // Switch framerate by tuning horizontal front porch
     kModeHFP,
+    // Switch framerate by tuning horizontal front porch and clock
+    kModeClockHFP,
+    // Switch framerate by tuning horizontal front porch and re-caculate clock
+    kModeHFPCalcClock,
     kModeMAX
+  };
+
+  /**
+   * struct DynamicFPSData - defines dynamic fps related data
+   * @hor_front_porch: horizontal front porch
+   * @hor_back_porch: horizontal back porch
+   * @hor_pulse_width: horizontal pulse width
+   * @clk_rate_hz: panel clock rate in HZ
+   * @fps: frames per second
+   */
+  struct DynamicFPSData {
+    uint32_t hor_front_porch;
+    uint32_t hor_back_porch;
+    uint32_t hor_pulse_width;
+    uint32_t clk_rate_hz;
+    uint32_t fps;
   };
 
   HWHDMI(BufferSyncHandler *buffer_sync_handler, HWInfoInterface *hw_info_intf);
@@ -84,6 +104,8 @@ class HWHDMI : public HWDevice {
                                     HWDisplayAttributes *attrib);
   bool IsSupportedS3DMode(HWS3DMode s3d_mode);
 
+  DisplayError GetDynamicFrameRateMode(uint32_t refresh_rate, uint32_t*mode,
+                                       DynamicFPSData *data, uint32_t *config_index);
   uint32_t hdmi_mode_count_;
   uint32_t hdmi_modes_[256];
   // Holds the hdmi timing information. Ex: resolution, fps etc.,
