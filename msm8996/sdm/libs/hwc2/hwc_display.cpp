CyanogenMod/android_hardware_qcom_display

commit d369ccd4bc54a465dbc4693706360a1416f674e7

8996/HWC2: Return this frame's retire fence
Changes the semantics of the retire fence to mean that it fires when
this frame has been picked up for composition (rather than when the
prior frame has been DMA'd to panel memory). This enables
SurfaceFlinger to use this fence to detect whether its prior
composition was actually picked up by hardware in time so that it can
propagate the backpressure of missed vsyncs back to the apps.

Bug: 29413700
Change-Id: Ibc3575c99f8649854ddc5d62decb904de4f66c48

msm8996/sdm/libs/hwc2/hwc_display.cpp
@@ -891,6 +891,7 @@ HWC2::Error HWCDisplay::PostCommitLayerStack(int32_t *out_retire_fence) {
       layer_stack_.retire_fence_fd = -1;
     }
     stored_retire_fence_ = layer_stack_.retire_fence_fd;
+    *out_retire_fence = layer_stack_.retire_fence_fd;
 
     if (dump_frame_count_) {
       dump_frame_count_--;

commit 47ac02696189725cfb4d056c4b2ba1fed2f647bb

hwc2: Use current frames retire fence only for the primary
Fixes regression with virtual displays introduced with
d369ccd4bc54a465dbc4693706360a1416f674e7
Change-Id: Id2fdcb01abd47b43ddad149d617828413422ecf3

msm8996/sdm/libs/hwc2/hwc_display.cpp
@@ -891,7 +891,9 @@ HWC2::Error HWCDisplay::PostCommitLayerStack(int32_t *out_retire_fence) {
       layer_stack_.retire_fence_fd = -1;
     }
     stored_retire_fence_ = layer_stack_.retire_fence_fd;
-    *out_retire_fence = layer_stack_.retire_fence_fd;
+    if (id_ == HWC_DISPLAY_PRIMARY) {
+      *out_retire_fence = layer_stack_.retire_fence_fd;
+    }
 
     if (dump_frame_count_) {
       dump_frame_count_--;
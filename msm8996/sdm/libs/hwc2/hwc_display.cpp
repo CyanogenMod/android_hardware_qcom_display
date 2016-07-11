47ac02696189725cfb4d056c4b2ba1fed2f647bb
Naseer Ahmed <naseer@codeaurora.org>
2016-07-11 19:26:24 -0400

    hwc2: Use current frames retire fence only for the primary
    Fixes regression with virtual displays introduced with
    d369ccd4bc54a465dbc4693706360a1416f674e7
    Change-Id: Id2fdcb01abd47b43ddad149d617828413422ecf3

--- msm8996/sdm/libs/hwc2/hwc_display.cpp
+++ msm8996/sdm/libs/hwc2/hwc_display.cpp
@@ -891,7 +891,9 @@ HWC2::Error HWCDisplay::PostCommitLayerStack(int32_t *out_retire_fence) {
       layer_stack_.retire_fence_fd = -1;
     }
     stored_retire_fence_ = layer_stack_.retire_fence_fd;
-    *out_retire_fence = layer_stack_.retire_fence_fd;
+    if (id_ == HWC_DISPLAY_PRIMARY) {
+      *out_retire_fence = layer_stack_.retire_fence_fd;
+    }
 
     if (dump_frame_count_) {
       dump_frame_count_--;
