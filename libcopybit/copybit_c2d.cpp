3b7a771c8dba698278e2e93c6c29ccfc6c11d195
Arne Coucheron <arco68@gmail.com>
2016-08-29 00:53:27 +0200

    display: Do not use GNU old-style field designators
    Improve code portability
    
    Change-Id: I3441c6ae2c10876877e79be579a24822d3f48882
    (cherry picked from commit 602d41acc65d40122710ff4486824a69a3cbb6a6)

--- libcopybit/copybit_c2d.cpp
+++ libcopybit/copybit_c2d.cpp
@@ -195,7 +195,7 @@ static int open_copybit(const struct hw_module_t* module, const char* name,
                         struct hw_device_t** device);
 
 static struct hw_module_methods_t copybit_module_methods = {
-    .open = open_copybit
+    .open = open_copybit,
 };
 
 /*
@@ -210,7 +210,7 @@ struct copybit_module_t HAL_MODULE_INFO_SYM = {
         .name = "QCT COPYBIT C2D 2.0 Module",
         .author = "Qualcomm",
         .methods = &copybit_module_methods
-    }
+    },
 };
 
 
