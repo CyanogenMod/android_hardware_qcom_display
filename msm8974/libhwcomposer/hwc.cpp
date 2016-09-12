73e0ce8b58f6b1ab2940468de03977b69d7c6a32
SpiritCroc <spiritcroc@gmail.com>
2016-09-12 15:45:45 +0200

    msm8974: Fix "use of GNU old-style field designator extension" errors
    Change-Id: Ia34f11746e65a06bc93eb3bc5673b66dcf092f98

--- msm8974/libhwcomposer/hwc.cpp
+++ msm8974/libhwcomposer/hwc.cpp
@@ -49,21 +49,21 @@ static int hwc_device_open(const struct hw_module_t* module,
                            struct hw_device_t** device);
 
 static struct hw_module_methods_t hwc_module_methods = {
-    open: hwc_device_open
+    .open = hwc_device_open,
 };
 
 hwc_module_t HAL_MODULE_INFO_SYM = {
-    common: {
-        tag: HARDWARE_MODULE_TAG,
-        version_major: 2,
-        version_minor: 0,
-        id: HWC_HARDWARE_MODULE_ID,
-        name: "Qualcomm Hardware Composer Module",
-        author: "CodeAurora Forum",
-        methods: &hwc_module_methods,
-        dso: 0,
-        reserved: {0},
-    }
+    .common = {
+        .tag = HARDWARE_MODULE_TAG,
+        .version_major = 2,
+        .version_minor = 0,
+        .id = HWC_HARDWARE_MODULE_ID,
+        .name = "Qualcomm Hardware Composer Module",
+        .author = "CodeAurora Forum",
+        .methods = &hwc_module_methods,
+        .dso = 0,
+        .reserved = {0},
+    },
 };
 
 /*
