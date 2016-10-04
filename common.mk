4da54a50d98faf19fad7ba0bea1d636f930540b8
Steve Kondik <steve@cyngn.com>
2016-10-04 13:47:07 -0700

    sdm: Always include kernel headers
    Change-Id: I63976a1f233f5f614501f02af301cd0215b362f4

--- common.mk
+++ common.mk
@@ -62,15 +62,13 @@ ifeq ($(TARGET_IS_HEADLESS),true)
     common_flags += -DTARGET_HEADLESS
 endif
 
-ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
 # This check is to pick the kernel headers from the right location.
 # If the macro above is defined, we make the assumption that we have the kernel
 # available in the build tree.
 # If the macro is not present, the headers are picked from hardware/qcom/msmXXXX
 # failing which, they are picked from bionic.
-    common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-    kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-endif
+common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
 ifeq ($(TARGET_USES_SDM_LEGACY),true)
     common_flags += -DSDM_LEGACY
